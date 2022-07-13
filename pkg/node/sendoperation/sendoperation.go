39ff2f88a9a8be97e1d27f240c0f991df8438eb1
Trottoir <31350013+Trottoir@users.noreply.github.com>
2022-07-13 14:52:01 +0200

    Replace secp256k1 ECDSA to Ed25519 EdDSA (#26)
    repalces secp256k1 signature by Ed25519 one.

--- pkg/node/sendoperation/sendoperation.go
+++ pkg/node/sendoperation/sendoperation.go
@@ -2,28 +2,20 @@ package sendoperation
 
 import (
 	"context"
-	"crypto/rand"
+	"crypto/ed25519"
 	"encoding/binary"
 	"fmt"
+	"strings"
 
-	"github.com/btcsuite/btcd/btcec/v2"
-	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"github.com/massalabs/thyra/pkg/node"
-	"lukechampine.com/blake3"
-
 	"github.com/massalabs/thyra/pkg/node/base58"
+	"lukechampine.com/blake3"
 )
 
-type sendOperationReqContent struct {
-	ExpirePeriod int64       `json:"expire_period"`
-	Fee          string      `json:"fee"`
-	Op           interface{} `json:"op"`
-	SenderPK     string      `json:"sender_public_key"`
-}
-
 type sendOperationsReq struct {
-	Content   sendOperationReqContent `json:"content"`
-	Signature string                  `json:"signature"`
+	SerializedContent JSONableSlice `json:"serialized_content"`
+	PublicKey         string        `json:"creator_public_key"`
+	Signature         string        `json:"signature"`
 }
 
 type Operation interface {
@@ -31,7 +23,19 @@ type Operation interface {
 	Message() []byte
 }
 
-func message(expiry uint64, fee uint64, senderPubKey []byte, operation Operation) []byte {
+type JSONableSlice []uint8
+
+func (u JSONableSlice) MarshalJSON() ([]byte, error) {
+	var result string
+	if u == nil {
+		result = "null"
+	} else {
+		result = strings.Join(strings.Fields(fmt.Sprintf("%d", u)), ",")
+	}
+	return []byte(result), nil
+}
+
+func message(expiry uint64, fee uint64, operation Operation) []byte {
 	msg := make([]byte, 0)
 	buf := make([]byte, binary.MaxVarintLen64)
 
@@ -43,9 +47,6 @@ func message(expiry uint64, fee uint64, senderPubKey []byte, operation Operation
 	n = binary.PutUvarint(buf, expiry)
 	msg = append(msg, buf[:n]...)
 
-	// public key
-	msg = append(msg, senderPubKey...)
-
 	// operation
 	msg = append(msg, operation.Message()...)
 
@@ -53,29 +54,17 @@ func message(expiry uint64, fee uint64, senderPubKey []byte, operation Operation
 }
 
 func sign(msg []byte, privKey []byte) ([]byte, error) {
-	digest := blake3.Sum256(msg)
-
-	var auxBytes [32]byte
-
-	if _, err := rand.Read(auxBytes[:]); err != nil {
-		return nil, err
-	}
-
-	signOpts := []schnorr.SignOption{schnorr.CustomNonce(auxBytes)}
-
-	pk, _ := btcec.PrivKeyFromBytes(privKey)
-
-	signature, err := schnorr.Sign(pk, digest[:], signOpts...)
-	if err != nil {
-		return nil, err
-	}
-
-	return signature.Serialize(), nil
+	signature := ed25519.Sign(privKey, msg)
+	return signature, nil
 }
 
 func Call(client *node.Client, expiry uint64, fee uint64, op Operation, pubKey []byte, privKey []byte) (string, error) {
+	msg := message(expiry, fee, op)
+
+	digest := blake3.Sum256(append(pubKey, msg...))
+
 	signature, err := sign(
-		message(expiry, fee, pubKey, op),
+		digest[:],
 		privKey,
 	)
 	if err != nil {
@@ -88,16 +77,14 @@ func Call(client *node.Client, expiry uint64, fee uint64, op Operation, pubKey [
 		[][]sendOperationsReq{
 			{
 				sendOperationsReq{
-					Content: sendOperationReqContent{
-						ExpirePeriod: int64(expiry),
-						Fee:          fmt.Sprint(fee),
-						Op:           op.Content(),
-						SenderPK:     base58.CheckEncode(pubKey),
-					},
-					Signature: base58.CheckEncode(signature),
+					SerializedContent: msg,
+					Signature:         base58.CheckEncode(signature),
+					PublicKey:         "P" + base58.VersionedCheckEncode(pubKey, 0),
 				},
 			},
-		})
+		},
+	)
+
 	if err != nil {
 		return "", err
 	}
