2e613911c5bc7d47423bbf1c16fda6f84f4ff505
thomas-senechal <thomas.senechal@pm.me>
2022-10-24 08:45:11 +0200

    Remove useless /usr/local/bin exist check.

--- scripts/linux_install.sh
+++ scripts/linux_install.sh
@@ -26,7 +26,6 @@ install_thyra () {
     curl -s -L "${BINARY_URL}_${arch}" -o thyra-server || fatal "binary download failed."
 
     chmod +x thyra-server || fatal "change to executable failed."
-    (ls /usr/local/bin/ &> /dev/null || sudo mkdir -p /usr/local/bin) || fatal "/usr/local/bin creation failed."
     sudo mv thyra-server /usr/local/bin/ || fatal "move to /usr/local/bin/ failed."
 }
 
