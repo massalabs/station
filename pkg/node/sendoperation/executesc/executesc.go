52c62ecfe06881d6fae10050e1c8d7685610271c
ASAPSegfault <45430752+ASAPSegfault@users.noreply.github.com>
2022-10-28 10:03:22 +0200

    Deploy Thyra on Innonet 15 #218
    Add datastore parameter to executeSC and update get_status endpoint JSON deserialisation pattern

--- pkg/node/sendoperation/executesc/executesc.go
+++ pkg/node/sendoperation/executesc/executesc.go
@@ -1,17 +1,19 @@
 package executesc
 
 import (
+	"bytes"
 	"encoding/binary"
+	"encoding/gob"
 	"fmt"
 )
 
 const ExecuteSCOpID = 3
 
 type OperationDetails struct {
-	Data     []byte `json:"data"`
-	MaxGas   uint64 `json:"max_gas"`
-	GasPrice string `json:"gas_price"`
-	Coins    string `json:"coins"`
+	Data      []byte                  `json:"data"`
+	MaxGas    uint64                  `json:"max_gas"`
+	GasPrice  string                  `json:"gas_price"`
+	DataStore map[[50]uint8][50]uint8 `json:"datastore"`
 }
 
 //nolint:tagliatelle
@@ -20,28 +22,28 @@ type Operation struct {
 }
 
 type ExecuteSC struct {
-	data     []byte
-	maxGas   uint64
-	gasPrice uint64
-	Coins    uint64
+	data      []byte
+	maxGas    uint64
+	gasPrice  uint64
+	dataStore map[[50]uint8][50]uint8
 }
 
-func New(data []byte, maxGas uint64, gasPrice uint64, coins uint64) *ExecuteSC {
+func New(data []byte, maxGas uint64, gasPrice uint64, dataStore map[[50]uint8][50]uint8) *ExecuteSC {
 	return &ExecuteSC{
-		data:     data,
-		maxGas:   maxGas,
-		gasPrice: gasPrice,
-		Coins:    coins,
+		data:      data,
+		maxGas:    maxGas,
+		gasPrice:  gasPrice,
+		dataStore: dataStore,
 	}
 }
 
 func (e *ExecuteSC) Content() interface{} {
 	return &Operation{
 		ExecuteSC: OperationDetails{
-			Data:     e.data,
-			MaxGas:   e.maxGas,
-			GasPrice: fmt.Sprint(e.gasPrice),
-			Coins:    fmt.Sprint(e.Coins),
+			Data:      e.data,
+			MaxGas:    e.maxGas,
+			GasPrice:  fmt.Sprint(e.gasPrice),
+			DataStore: e.dataStore,
 		},
 	}
 }
@@ -58,10 +60,6 @@ func (e *ExecuteSC) Message() []byte {
 	nbBytes = binary.PutUvarint(buf, e.maxGas)
 	msg = append(msg, buf[:nbBytes]...)
 
-	// Coins
-	nbBytes = binary.PutUvarint(buf, e.Coins)
-	msg = append(msg, buf[:nbBytes]...)
-
 	// GasPrice
 	nbBytes = binary.PutUvarint(buf, e.gasPrice)
 	msg = append(msg, buf[:nbBytes]...)
@@ -71,5 +69,25 @@ func (e *ExecuteSC) Message() []byte {
 	msg = append(msg, buf[:nbBytes]...)
 	msg = append(msg, e.data...)
 
+	// datastore
+
+	bytesBuffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(bytesBuffer)
+	err := encoder.Encode(e.dataStore)
+	if err != nil {
+		fmt.Print("Panicking...\n")
+		panic(err)
+	}
+	binary.PutUvarint(buf, uint64(len(e.data)))
+	bytesBufferLength := len(bytesBuffer.Bytes())
+	nbBytes = binary.PutUvarint(buf, uint64(bytesBufferLength))
+	msg = append(msg, buf[:nbBytes]...)
+
+	fmt.Print("bytesBuffer len : \n")
+	fmt.Print(bytesBufferLength)
+	fmt.Print("\n")
+	msg = append(msg, bytesBuffer.Bytes()...)
+	//fmt.Print(msg)
+	fmt.Print("Returning msg...\n")
 	return msg
 }
