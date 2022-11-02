ec44faca876907002769ea603ad49e6af80938ba
ASAPSegfault <45430752+ASAPSegfault@users.noreply.github.com>
2022-11-02 09:12:25 +0100

    Deploy Thyra on Innonet 15 #218
    Cleaned and refactored executeSC call

--- pkg/node/sendoperation/executesc/executesc.go
+++ pkg/node/sendoperation/executesc/executesc.go
@@ -10,11 +10,10 @@ import (
 const ExecuteSCOpID = 3
 
 type OperationDetails struct {
-	Data      []byte                `json:"data"`
-	MaxGas    uint64                `json:"max_gas"`
-	GasPrice  string                `json:"gas_price"`
-	DataStore map[[3]uint8][3]uint8 `json:"datastore"`
-	Coins     string                `json:"coins"`
+	Data      []byte               `json:"data"`
+	MaxGas    uint64               `json:"max_gas"`
+	GasPrice  string               `json:"gas_price"`
+	DataStore map[[3]uint8][]uint8 `json:"datastore"`
 }
 
 //nolint:tagliatelle
@@ -26,10 +25,12 @@ type ExecuteSC struct {
 	data      []byte
 	maxGas    uint64
 	gasPrice  uint64
-	dataStore map[[3]uint8][3]uint8
+	dataStore map[[3]uint8][]uint8
 }
 
-func New(data []byte, maxGas uint64, gasPrice uint64, coins uint64, dataStore map[[3]uint8][3]uint8) *ExecuteSC {
+func New(data []byte, maxGas uint64, gasPrice uint64, coins uint64, dataStore map[[3]uint8][]uint8) *ExecuteSC {
+	gob.Register(map[[3]uint8]interface{}{})
+
 	return &ExecuteSC{
 		data:      data,
 		maxGas:    maxGas,
@@ -73,86 +74,29 @@ func (e *ExecuteSC) Message() []byte {
 	// datastore
 	// Number of entries in the datastore
 	nbBytes = binary.PutUvarint(buf, uint64(len(e.dataStore)))
-	fmt.Print(uint64(len(e.dataStore)))
-	fmt.Print("\n")
 	msg = append(msg, buf[:nbBytes]...)
 
 	for key, value := range e.dataStore {
-		fmt.Printf("key[%d] value[%d]\n", key, value)
-		// Key length
-		// nbBytes = binary.PutUvarint(buf, len(key))
-		// msg = append(msg, key...)
-		bytesBuffer := new(bytes.Buffer)
-		encoder := gob.NewEncoder(bytesBuffer)
-		err := encoder.Encode(len(key))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Print(len(key))
-		fmt.Print("\n")
-		fmt.Print(bytesBuffer.Bytes())
-		fmt.Print("\n")
-		fmt.Print(len(bytesBuffer.Bytes()))
-		fmt.Print("\n")
-		msg = append(msg, 26)
-
-		// Key in bytes
-		bytesBuffer = new(bytes.Buffer)
-		encoder = gob.NewEncoder(bytesBuffer)
-		err = encoder.Encode(key)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Print(len(bytesBuffer.Bytes()))
-		fmt.Print("\n")
-		msg = append(msg, bytesBuffer.Bytes()...)
-
-		// Value length
-		// nbBytes = binary.PutUvarint(buf, uint64(len(value)))
-		// msg = append(msg, buf[:nbBytes]...)
-		bytesBuffer = new(bytes.Buffer)
-		encoder = gob.NewEncoder(bytesBuffer)
-		err = encoder.Encode(len(value))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Print(len(value))
-		fmt.Print("\n")
-		fmt.Print(bytesBuffer.Bytes())
-		fmt.Print("\n")
-		fmt.Print(len(bytesBuffer.Bytes()))
-		fmt.Print("\n")
-
-		msg = append(msg, 26)
-
-		// Value in bytes
-		bytesBuffer = new(bytes.Buffer)
-		encoder = gob.NewEncoder(bytesBuffer)
-		err = encoder.Encode(value)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Print(len(bytesBuffer.Bytes()))
-		fmt.Print("\n")
-		msg = append(msg, bytesBuffer.Bytes()...)
+		compactAndAppendBytes(&msg, key)
+		compactAndAppendBytes(&msg, value)
 	}
 
-	// bytesBuffer := new(bytes.Buffer)
-	// encoder := gob.NewEncoder(bytesBuffer)
-	// err := encoder.Encode(e.dataStore)
-	// if err != nil {
-	// 	fmt.Print("Panicking...\n")
-	// 	panic(err)
-	// }
-	// bytesBufferLength := len(bytesBuffer.Bytes())
-	// nbBytes = binary.PutUvarint(buf, uint64(bytesBufferLength))
-	// msg = append(msg, buf[:nbBytes]...)
-
-	// fmt.Print("bytesBuffer len : \n")
-	// fmt.Print(bytesBufferLength)
-	// fmt.Print("\n")
-	// msg = append(msg, bytesBuffer.Bytes()...)
-	//fmt.Print(msg)
-	fmt.Print("Returning msg...\n")
 	return msg
+}
+
+func compactAndAppendBytes(msg *[]byte, value interface{}) {
+	buf := make([]byte, binary.MaxVarintLen64)
+	bytesBuffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(bytesBuffer)
+	err := encoder.Encode(value)
+
+	if err != nil {
+		panic(err)
+	}
+
+	nbBytes := binary.PutUvarint(buf, uint64(bytesBuffer.Len()))
+	// Value length
+	*msg = append(*msg, buf[:nbBytes]...)
+	// Value in bytes
+	*msg = append(*msg, bytesBuffer.Bytes()...)
 }
