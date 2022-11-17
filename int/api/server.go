9f6127bacefe9e79d90a05eb1221b0d17139e024
Sebastien Valla <83454858+SebastienValla@users.noreply.github.com>
2022-11-17 10:05:53 +0100

    change TESTNET by default

--- int/api/server.go
+++ int/api/server.go
@@ -27,7 +27,7 @@ func parseFlags(server *restapi.Server) {
 
 	certFilePtr := flag.String("tls-certificate", "", "path to certificate file")
 	keyFilePtr := flag.String("tls-key", "", "path to key file")
-	massaNodeServerPtr := flag.String("node-server", "INNONET", `Massa node that Thyra connects to. 
+	massaNodeServerPtr := flag.String("node-server", "TESTNET", `Massa node that Thyra connects to. 
 	Can be an IP address, a URL or one of the following values: 'TESTNET', 'LABNET', 'INNONET' or LOCALHOST`)
 
 	flag.Parse()
