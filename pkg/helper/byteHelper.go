352b178e3027dbfe44706087f5379906994a7f1e
0xZzzZ <JeanCrypto619@gmail.com>
2022-12-01 10:59:14 +0100

    Clean Code and Update Helper

--- pkg/helper/byteHelper.go
+++ pkg/helper/byteHelper.go
@@ -1,6 +1,9 @@
 package helper
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"unicode/utf16"
+)
 
 func Uint64ToByteArrayU8(u64 uint64) []byte {
 	encode := 8
@@ -20,4 +23,20 @@ func ByteArrayToUint64(byteArray []byte) uint64 {
 
 func ByteArrayToUint32(byteArray []byte) uint32 {
 	return binary.LittleEndian.Uint32(byteArray)
+}
+
+func EncodeUint8ToUTF16String(numberToEncode uint32) string {
+	//nolint:gomnd
+	buffer := make([]byte, 4)
+	binary.LittleEndian.PutUint32(buffer, numberToEncode)
+	//nolint:gomnd
+	runesBuffer := make([]rune, 4)
+
+	for i := 0; i < len(buffer); i++ {
+		runesBuffer[i] = utf16.Decode([]uint16{uint16(buffer[i])})[0]
+	}
+
+	encodedString := string(runesBuffer)
+
+	return encodedString
 }
--- pkg/onchain/dns/dns.go
+++ pkg/onchain/dns/dns.go
@@ -1,7 +1,6 @@
 package dns
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 
@@ -30,61 +29,21 @@ func Resolve(client *node.Client, name string) (string, error) {
 	return string(entry.CandidateValue), nil
 }
 
-type setApproval struct {
-	Operator string `json:"operator"`
-	Approved bool   `json:"approved"`
-}
-
-type setRecord struct {
-	Name    []byte `json:"name"`
-	Address string `json:"address"`
-}
-
 func SetRecord(client *node.Client, wallet wallet.Wallet, url string, smartContract string) (string, error) {
 	addr, _, err := base58.VersionedCheckDecode(DNSRawAddress[1:])
 	if err != nil {
 		return "", fmt.Errorf("checking address '%s': %w", DNSRawAddress[1:], err)
 	}
 
-	rec := setRecord{
-		Name:    helper.StringToByteArray(url),
-		Address: smartContract,
-	}
-
-	param, err := json.Marshal(rec)
-	if err != nil {
-		return "", fmt.Errorf("marshalling '%+v': %w", rec, err)
-	}
-
-	result, err := onchain.CallFunction(client, wallet, addr, "setResolver", param, sendoperation.OneMassa)
-	if err != nil {
-		return "", fmt.Errorf("calling setResolver with '%+v' at '%s': %w", param, addr, err)
-	}
-
-	return result, nil
-}
-
-// DEAD CODE ??
-func SetRecordManager(client *node.Client, wallet wallet.Wallet) (string, error) {
-	addr, _, err := base58.VersionedCheckDecode(DNSRawAddress[1:])
-	if err != nil {
-		return "", fmt.Errorf("checking address '%s': %w", DNSRawAddress[1:], err)
-	}
-
 	// Set Resolver prepare data
-	appr := &setApproval{
-		Operator: wallet.Address,
-		Approved: true,
-	}
+	rec := []byte(helper.EncodeUint8ToUTF16String(uint32(len(url))))
+	rec = append(rec, helper.StringToByteArray(url)...)
+	rec = append(rec, helper.EncodeUint8ToUTF16String(uint32(len(smartContract)))...)
+	rec = append(rec, helper.StringToByteArray(smartContract)...)
 
-	param, err := json.Marshal(appr)
+	result, err := onchain.CallFunction(client, wallet, addr, "setResolver", rec, sendoperation.OneMassa)
 	if err != nil {
-		return "", fmt.Errorf("marshalling '%+v': %w", appr, err)
-	}
-
-	result, err := onchain.CallFunction(client, wallet, addr, "setApprovalForAll", param, sendoperation.OneMassa)
-	if err != nil {
-		return "", fmt.Errorf("calling setApprovalForAll with '%+v' at '%s': %w", param, addr, err)
+		return "", fmt.Errorf("calling setResolver with '%+v' at '%s': %w", rec, addr, err)
 	}
 
 	return result, nil
