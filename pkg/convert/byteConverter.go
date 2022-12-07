db6e61facf4b684844167f0deff80578ded233f3
ASAPSegfault <45430752+ASAPSegfault@users.noreply.github.com>
2022-12-07 18:15:48 +0100

    Apply suggestions from code review
    Co-authored-by: Grégory Libert <greg_libert@yahoo.fr>

--- pkg/convert/byteConverter.go
+++ pkg/convert/byteConverter.go
@@ -8,7 +8,8 @@ import (
 const bytesPerUint64 = 8
 
 // Encode uint64 to byte array.
-func FromU64(u64 uint64) (bytes []byte) {
+func U64ToBytes(nb int) (bytes []byte) {
+        u64 := uint64(nb)
 	bytes = make([]byte, bytesPerUint64)
 	binary.LittleEndian.PutUint64(bytes, u64)
 
@@ -18,32 +19,32 @@ func FromU64(u64 uint64) (bytes []byte) {
 const bytesPerUint32 = 4
 
 // Encode uint32 to byte array.
-func FromU32(u32 uint32) (bytes []byte) {
+func U32ToBytes(nb int) (bytes []byte) {
+        u32 := uint32(nb)
 	bytes = make([]byte, bytesPerUint32)
 	binary.LittleEndian.PutUint32(bytes, u32)
 
 	return
 }
 
-func EncodeStringToByteArray(str string) []byte {
+func StringToBytes(str string) []byte {
         //let's start by encoding the string length
 	lenBytes := FromU32(len(str))
-	//nolint:gomnd
-	runesBuffer := make([]rune, 4)
+	buffer := make([]rune, bytesPerUint32)
 
 	for i := 0; i < len(buffer); i++ {
 		runesBuffer[i] = utf16.Decode([]uint16{uint16(buffer[i])})[0]
 	}
 
-	encodedString := string(runesBuffer)
-	slice := []byte(encodedString)
+	encodedLength := string(runesBuffer)
+	encodedLengthBytes := []byte(encodedLength)
 
 	return append(slice, str...)
 }
 
-func RemoveStringEncodingPrefix(entry []byte) string {
+func BytesToString (entry []byte) string {
 	prefix := 4 // 4 first bytes representing the Length of the string
-	entryWithoutPrefix := entry[prefix:]
+	content := entry[bytesPerUint32:] //content is always prefixed by its size encoded using a u32
 
 	return string(entryWithoutPrefix)
 }
