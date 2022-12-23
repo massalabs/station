c9dd2f7c98dfa620a1f0a44300e7d190845a766e
0xZzzZ <JeanCrypto619@gmail.com>
2022-12-23 12:01:51 +0100

    Update Linter

--- pkg/cert/certHandler.go
+++ pkg/cert/certHandler.go
@@ -19,18 +19,18 @@ import (
 const rootName = "rootCA.pem"
 const rootKeyName = "rootCA-key.pem"
 
-// CA Path
+// CAROOT ...
 var CAROOT = getCAROOT()
 
-// CA certificate and key
+// CA certificate and key.
 var caCert *x509.Certificate
 var caKey crypto.PrivateKey
 
-// Private Key for the certificate
+// Private Key for the certificate.
 var priv *rsa.PrivateKey
 
-// Static data for the certificate
-var tempCertificate x509.Certificate = x509.Certificate{
+// Static data for the certificate.
+var tempCertificate = x509.Certificate{
 	SerialNumber: randomSerialNumber(),
 	Subject: pkix.Name{
 		Organization: []string{"thyra dynamically generated"},
@@ -40,21 +40,26 @@ var tempCertificate x509.Certificate = x509.Certificate{
 }
 
 func getCa() (*x509.Certificate, crypto.PrivateKey) {
-	// Get the CA root certificate
+	// Get the CA root certificate.
 	if CAROOT == "" {
 		log.Fatalln("ERROR: failed to find the default CA location, set one as the CAROOT env var")
 	}
-	// Load the CA certificate and key
+	// Load the CA certificate and key.
 	return loadCA()
 }
 
-func generatePrivAndPubKey() (crypto.PrivateKey, crypto.PublicKey) {
-	// Generate a private key
+func generatePrivAndPubKey() (*rsa.PrivateKey, crypto.PublicKey) {
+	// Generate a private key.
 	var err error
+
 	if priv != nil {
 		return priv, priv.Public()
 	}
-	priv, err = rsa.GenerateKey(rand.Reader, 2048)
+
+	const random = 2048
+
+	priv, err = rsa.GenerateKey(rand.Reader, random)
+
 	fatalIfErr(err, "failed to generate private key")
 
 	return priv, priv.Public()
@@ -62,13 +67,14 @@ func generatePrivAndPubKey() (crypto.PrivateKey, crypto.PublicKey) {
 
 func GenerateCertificate(serverName string) ([]byte, crypto.PrivateKey, error) {
 	caCert, caKey = getCa()
+
 	if len(serverName) == 0 {
 		panic("error:ServerName is Empty.")
 	}
 
 	priv, pub := generatePrivAndPubKey()
 
-	// Create the template for the certificate
+	// Create the template for the certificate.
 	expiration := time.Now().AddDate(0, 1, 0)
 	tpl := &tempCertificate
 	tpl = &x509.Certificate{
@@ -79,53 +85,65 @@ func GenerateCertificate(serverName string) ([]byte, crypto.PrivateKey, error) {
 		NotAfter:  expiration,
 		DNSNames:  append(tpl.DNSNames, serverName),
 	}
-	// Create the certificate
+	// Create the certificate.
 	cert, err := x509.CreateCertificate(rand.Reader, tpl, caCert, pub, caKey)
 
 	return cert, priv, err
 }
 
-// Get the CA root certificate depending on your OS
+// Get the CA root certificate depending on your OS.
 func getCAROOT() string {
 	if env := os.Getenv("CAROOT"); env != "" {
 		return env
 	}
+
 	var dir string
+
 	switch {
 	case runtime.GOOS == "windows":
+
 		dir = os.Getenv("LocalAppData")
+
 	case os.Getenv("XDG_DATA_HOME") != "":
+
 		dir = os.Getenv("XDG_DATA_HOME")
+
 	case runtime.GOOS == "darwin":
+
 		dir = os.Getenv("HOME")
 		if dir == "" {
-			// $HOME is not set
-			// return empty string when CAROOT is not set
+			// $HOME is not set.
+			// return empty string when CAROOT is not set.
 			return ""
 		}
+
 		dir = filepath.Join(dir, "Library", "Application Support")
+
 	case runtime.GOOS == "linux":
 		dir = os.Getenv("HOME")
 		if dir == "" {
-			// $HOME is not set
-			// return empty string when CAROOT is not set
+			// $HOME is not set.
+			// return empty string when CAROOT is not set.
 			return ""
 		}
-	default: // Unix
+	default: // Unix.
 		log.Fatalln("ERROR: OS not supported please contact support")
 	}
+
 	return filepath.Join(dir, "mkcert")
 }
 
-// Get the CA root key or create a new one
+// Get the CA root key or create a new one.
 func loadCA() (*x509.Certificate, crypto.PrivateKey) {
 
 	if !pathExists(filepath.Join(CAROOT, rootName)) {
 		log.Fatalln("ERROR: failed to find the CA Root path at: " + filepath.Join(CAROOT, rootName) + "")
 	}
+
 	certPEMBlock, err := os.ReadFile(filepath.Join(CAROOT, rootName))
 
 	fatalIfErr(err, "failed to read the CA certificate")
+
 	certDERBlock, _ := pem.Decode(certPEMBlock)
 	if certDERBlock == nil || certDERBlock.Type != "CERTIFICATE" {
 		log.Fatalln("ERROR: failed to read the CA certificate: unexpected content")
@@ -139,26 +157,31 @@ func loadCA() (*x509.Certificate, crypto.PrivateKey) {
 
 	keyPEMBlock, err := ioutil.ReadFile(filepath.Join(CAROOT, rootKeyName))
 	fatalIfErr(err, "failed to read the CA key")
+
 	keyDERBlock, _ := pem.Decode(keyPEMBlock)
 	if keyDERBlock == nil || keyDERBlock.Type != "PRIVATE KEY" {
 		log.Fatalln("ERROR: failed to read the CA key: unexpected content")
 	}
 	caKey, err = x509.ParsePKCS8PrivateKey(keyDERBlock.Bytes)
 	fatalIfErr(err, "failed to parse the CA key")
+
 	return caCert, caKey
 }
 
 // randomSerialNumber generates a random serial number for the certificate.
 func randomSerialNumber() *big.Int {
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	const bits = 128 // 128 bits
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), bits)
 	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
 	fatalIfErr(err, "failed to generate serial number")
+
 	return serialNumber
 }
 
-// return true if path exist
+// return true if path exist.
 func pathExists(path string) bool {
 	_, err := os.Stat(path)
+
 	return err == nil
 }
 
