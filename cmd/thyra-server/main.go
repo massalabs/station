505acaa0e455da1a64a51d8379c70e5adb529654
thomas-senechal <thomas.senechal@pm.me>
2023-01-04 10:42:08 +0100

    Change default NodeServer.

--- cmd/thyra-server/main.go
+++ cmd/thyra-server/main.go
@@ -12,25 +12,27 @@ import (
 	"github.com/massalabs/thyra/pkg/config"
 )
 
+const (
+	defaultHttpPort   = 80
+	defaultHttpsPort  = 443
+	defaultNodeServer = "INNONET"
+)
+
 //go:embed version.txt
 var version string
 
 func ParseFlags() api.StartServerFlags {
-	const httpPort = 80
-
-	const httpsPort = 443
-
 	var flags api.StartServerFlags
 
 	configDir, _ := config.GetConfigDir()
 	defaultCertFile := path.Join(configDir, "certs", "cert.pem")
 	defaultCertKeyFile := path.Join(configDir, "certs", "cert-key.pem")
 
-	flag.IntVar(&flags.Port, "http-port", httpPort, "HTTP port to listen to")
-	flag.IntVar(&flags.TLSPort, "https-port", httpsPort, "HTTPS port to listen to")
+	flag.IntVar(&flags.Port, "http-port", defaultHttpPort, "HTTP port to listen to")
+	flag.IntVar(&flags.TLSPort, "https-port", defaultHttpsPort, "HTTPS port to listen to")
 	flag.StringVar(&flags.TLSCertificate, "tls-certificate", defaultCertFile, "path to certificate file")
 	flag.StringVar(&flags.TLSCertificateKey, "tls-key", defaultCertKeyFile, "path to key file")
-	flag.StringVar(&flags.MassaNodeServer, "node-server", "TESTNET", `Massa node that Thyra connects to. 
+	flag.StringVar(&flags.MassaNodeServer, "node-server", defaultNodeServer, `Massa node that Thyra connects to. 
 	Can be an IP address, a URL or one of the following values: 'TESTNET', 'LABNET', 'INNONET' or LOCALHOST`)
 	flag.StringVar(&flags.DNSAddress, "dns-address", "", "Address of the DNS contract on the blockchain")
 	flag.BoolVar(&flags.Version, "version", false, "Print version info and exit")
