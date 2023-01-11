991367100f97977b93986ef5f9922c33636df806
Sebastien Valla <sv@massa.net>
2023-01-11 18:34:55 +0100

    linter

--- pkg/onchain/storage/storage.go
+++ pkg/onchain/storage/storage.go
@@ -147,6 +147,7 @@ func saveInCache(filePath string, content []byte) {
 
 	defer file.Close()
 
+	//nolint:gofumpt
 	err = os.WriteFile(filePath, content, 0600)
 	if err != nil {
 		log.Printf("error saving zipfile: %v\n", err)
