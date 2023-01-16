0889672d5337e2edd725fad44dda25663e03bf7c
0xMazout <lf@massa.net>
2023-01-16 12:15:09 +0100

    Add assets Update Home

--- web/thyra-frontend/src/pages/Home/home.tsx
+++ web/thyra-frontend/src/pages/Home/home.tsx
@@ -48,15 +48,15 @@ function home({}: Props) {
     // Map over the plugins and display them in a list
     const pluginList = plugins.map((plugin) => {
         return (
-            <button className="flex flex-wrap bg-slate-600 rounded-lg p-5 m-5 " key={plugin.name}>
+            <button className="flex flex-wrap rounded-lg p-5 m-5" key={plugin.name}>
                 <div className="mx-auto">
                     <div className="tooltip" data-tip={plugin.description}>
                         <img
-                            className="max-w-12 max-h-12 self-center bg-slate-800"
+                            className="w-9 h-9 self-center bg-slate-800"
                             src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/180e697b-a457-4ecf-9691-6192ba7eddec/MASSA_LIGHT.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230113%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230113T135113Z&X-Amz-Expires=86400&X-Amz-Signature=3a067257d5f862d6c3e215fb22eb61c40b944b64b93abbc5a85b8f930ed35dc4&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22MASSA_LIGHT.png%22&x-id=GetObject"
                         ></img>
                     </div>
-                    <h1 className="text-xl font-bold text-center text-white">{plugin.name}</h1>
+                    <h1 className="text-xs text-center text-white">{plugin.name}</h1>
                 </div>
             </button>
         );
@@ -75,13 +75,7 @@ function home({}: Props) {
             <div className="flex">
                 <div className="">
                     <p className="text-xl p-5 text-white">
-                        Welcome to Thyra, the best way to manage your{" "}
-                        <img
-                            className="max-w-5 max-h-5 inline-flex"
-                            src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/8f7fd86a-a417-4a0c-ae19-2f6d4c3212e1/massa_logomark.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230113%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230113T132953Z&X-Amz-Expires=86400&X-Amz-Signature=09450832b4c17520a7b91fc9e22e20473239f8c43dada436eb122a3af6068a50&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22massa_logomark.png%22&x-id=GetObject"
-                            alt="Massa Logo"
-                        />{" "}
-                        Tokens and Plugins.
+                        θύρα (thýra) in ancient Greek means door, entrance. To pronounce "thoo-rah".
                     </p>
                 </div>
             </div>
@@ -91,12 +85,12 @@ function home({}: Props) {
                     src="https://static.wixstatic.com/media/788ebc_5768179e35704336b41bce4f9cf246ea~mv2.jpg/v1/fill/w_560,h_522,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/788ebc_5768179e35704336b41bce4f9cf246ea~mv2.jpg"
                     alt="Thyra Logo"
                 />
-                <p className="text-center text-3xl font-bold text-white">
-                    A gateway to Massa Blockchain
+                <p className="text-center text-4xl font text-white">
+                    A gateway to Massa blockchain
                 </p>
             </div>
             {/* Display the plugins in a grid */}
-            <div className="m-4 grid mx-auto w-fit grid-cols-2 rounded-lg sm:grid-cols-3">
+            <div className="m-4 grid mx-auto w-fit grid-cols-2 rounded-lg sm:grid-cols-4">
                 {pluginList}
             </div>
         </div>
