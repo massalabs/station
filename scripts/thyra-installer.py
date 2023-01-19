d39f6b05ebe71fc6d10a16c578bccbbe76a55bc0
Thomas Sénéchal <44696378+thomas-senechal@users.noreply.github.com>
2023-01-19 10:56:46 +0100

    1 click install macos (#404)
    adds one-click install for MacOS

--- scripts/thyra-installer.py
+++ scripts/thyra-installer.py
@@ -1,3 +1,5 @@
+import platform
+import ssl
 import urllib.request
 import subprocess
 import shutil
@@ -10,6 +12,8 @@ from tarfile import ReadError
 
 import logging
 import os
+
+# This file is to be bundled with pyinstaller in order to produce a .exe that can run on Windows without Python installed.
 
 # set up logging to file
 logging.basicConfig(level=logging.INFO,
@@ -29,31 +33,73 @@ logging.getLogger('').addHandler(console)
 
 logging.info('Starting thyra installer . . .')
 
-# This file is to be bundled with pyinstaller in order to produce a .exe that can run on Windows without Python installed.
-THYRA_URL = "https://github.com/massalabs/thyra/releases/latest/download/thyra-server_windows_amd64"
-THYRA_FILENAME = "thyra-server.exe"
-THYRA_APP = "https://github.com/massalabs/Thyra-Menu-Bar-App/releases/latest/download/ThyraApp_windows-amd64.exe"
-THYRA_APP_FILENAME = "ThyraApp_windows-amd64.exe"
-THYRA_CONFIG_FOLDER_PATH = os.path.expanduser("~") + "\\.config\\thyra"
+# General
+THYRA_SERVER_URL = ""
+THYRA_SERVER_FILENAME = ""
+THYRA_APP_URL = ""
+THYRA_APP_FILENAME = ""
+THYRA_CONFIG_FOLDER_PATH = os.path.join(os.path.expanduser("~"), ".config", "thyra")
+
+USER_HOME_FOLDER = os.path.expanduser("~")
+
+# Windows // Acrylic
 ACRYLIC_DNS_PROXY_URL = "https://sourceforge.net/projects/acrylic/files/Acrylic/2.1.1/Acrylic-Portable.zip/download"
 ACRYLIC_DNS_PROXY_FILENAME = "Acrylic-Portable.zip"
 ACRYLIC_HOST_FILE = "AcrylicHosts.txt"
 DEFAULT_ACRYLIC_PATH = "C:\Program Files (x86)\Acrylic DNS Proxy"
+
+# Certifications
 MKCERT_URL = "https://dl.filippo.io/mkcert/latest?for=windows/amd64"
 MKCERT_FILENAME = "mkcert.exe"
-CERTIFICATIONS_FOLDER = THYRA_CONFIG_FOLDER_PATH + "\\certs"
-USER_HOME_FOLDER = os.path.expanduser("~")
+CERTIFICATIONS_FOLDER = os.path.join(THYRA_CONFIG_FOLDER_PATH, "certs")
 
-def downloadFile(url, filename):
-    logging.info("Downloading " + filename + "...")
-    try:
-        with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
-    except URLError as err:
-        logging.info("Failed to download " + filename + " :")
-        printErrorAndExit(err)
-    logging.info(filename + " downloaded successfully")
+# Global variables
+def setThyraGlobals():
+    global THYRA_SERVER_URL, THYRA_SERVER_FILENAME
 
+    if platform.system() == "Windows":
+        THYRA_SERVER_FILENAME = "thyra-server.exe"
+        THYRA_SERVER_URL = "https://github.com/massalabs/thyra/releases/latest/download/thyra-server_windows_amd64"
+    elif platform.system() == "Darwin":
+        THYRA_SERVER_FILENAME = "thyra-server"
+        if platform.machine() == "arm64":
+            THYRA_SERVER_URL = "https://github.com/massalabs/thyra/releases/latest/download/thyra-server_darwin_arm64"
+        elif platform.machine() == "x86_64":
+            THYRA_SERVER_URL = "https://github.com/massalabs/thyra/releases/latest/download/thyra-server_darwin_amd64"
+    else:
+        printErrorAndExit("Unsupported platform: " + platform.system())
+
+def setThyraAppGlobals():
+    global THYRA_APP_URL, THYRA_APP_FILENAME
+
+    if platform.system() == "Windows":
+            THYRA_APP_FILENAME = "thyra-app.exe"
+            THYRA_APP_URL = "https://github.com/massalabs/Thyra-Menu-Bar-App/releases/latest/download/ThyraApp_windows-amd64.exe"
+    elif platform.system() == "Darwin":
+            THYRA_APP_FILENAME = "thyra-app"
+            if platform.machine() == "arm64":
+                THYRA_APP_URL = "https://github.com/massalabs/Thyra-Menu-Bar-App/releases/latest/download/ThyraApp_darwin-arm64"
+            elif platform.machine() == "x86_64":
+                THYRA_APP_URL = "https://github.com/massalabs/Thyra-Menu-Bar-App/releases/latest/download/ThyraApp_darwin-amd64"
+    else:
+        printErrorAndExit("Unsupported platform: " + platform.system())
+
+def setMKCertGlobals():
+    global MKCERT_URL, MKCERT_FILENAME
+
+    if platform.system() == "Windows":
+            MKCERT_URL = "https://dl.filippo.io/mkcert/latest?for=windows/amd64"
+            MKCERT_FILENAME = "mkcert.exe"
+    elif platform.system() == "Darwin":
+            MKCERT_FILENAME = "mkcert"
+            if platform.machine() == "arm64":
+                MKCERT_URL = "https://dl.filippo.io/mkcert/latest?for=darwin/arm64"
+            elif platform.machine() == "x86_64":
+                MKCERT_URL = "https://dl.filippo.io/mkcert/latest?for=darwin/amd64"
+    else:
+        printErrorAndExit("Unsupported platform: " + platform.system())
+
+# Windows
 def unzipAcrylic():
     try: 
         os.mkdir(DEFAULT_ACRYLIC_PATH)
@@ -67,17 +113,6 @@ def unzipAcrylic():
         logging.info("Acrylic unzipped")
     except ReadError as err:
         printErrorAndExit(err)
-
-def executeOSCommandOrFile(command, decodeBinary):
-    process = subprocess.Popen(command,
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE,
-                            universal_newlines=decodeBinary)
-    stdout, stderr = process.communicate()
-
-    if stderr != None and stderr != "" and process.returncode != 0:
-        printErrorAndExit("Error encountered while executing : " + str(command) + " :\n" + stderr)
-    return stdout
 
 def setupDNS():
     commandOutput = executeOSCommandOrFile("wmic nic where \"netenabled=true\" get netconnectionID", True)
@@ -106,22 +141,27 @@ def configureAcrylic():
     executeOSCommandOrFile("NET STOP AcrylicDNSProxySvc", True)
     executeOSCommandOrFile("NET START AcrylicDNSProxySvc", True)
 
-def setupMkCerts():
+def generateCertificate():
     if os.path.isdir(THYRA_CONFIG_FOLDER_PATH) is False:
         try:
             os.mkdir(THYRA_CONFIG_FOLDER_PATH)
-            os.mkdir(THYRA_CONFIG_FOLDER_PATH + "\\certs")
         except OSError as err:
-            printErrorAndExit(err)
+            printErrorAndExit("Error while creating config folder: " + err)
+
+    if os.path.isdir(CERTIFICATIONS_FOLDER) is False:
+        try:
+            os.mkdir(CERTIFICATIONS_FOLDER)
+        except OSError as err:
+            printErrorAndExit("Error while creating certificates folder: " + err)
 
     downloadFile(MKCERT_URL, MKCERT_FILENAME)
-    executeOSCommandOrFile([os.getcwd() + "\\" + MKCERT_FILENAME, "--install"], False)
+    os.chmod(MKCERT_FILENAME, 755)
+
+    executeOSCommandOrFile([os.path.join(os.getcwd(), MKCERT_FILENAME), "--install"], False)
     executeOSCommandOrFile([
-        os.getcwd() + "\\" + MKCERT_FILENAME,
-        "--cert-file",
-        CERTIFICATIONS_FOLDER + "\\cert.pem",
-        "--key-file",
-        CERTIFICATIONS_FOLDER  + "\\cert-key.pem",
+        os.path.join(os.getcwd() , MKCERT_FILENAME),
+        "--cert-file", os.path.join(CERTIFICATIONS_FOLDER , "cert.pem"),
+        "--key-file", os.path.join(CERTIFICATIONS_FOLDER, "cert-key.pem"),
         "my.massa"], False)
     try:
         os.remove(MKCERT_FILENAME)
@@ -132,38 +172,117 @@ def setupMkCerts():
 def isAdmin():
     return ctypes.windll.shell32.IsUserAnAdmin() != 0
 
+# MacOS
+def configureDNSMasq():
+    print("Configuring DNSMasq...")
+    
+    executeOSCommandOrFile("sudo bash -c 'echo ""address=/.massa/127.0.0.1"" > $(brew --prefix)/etc/dnsmasq.d/massa.conf'", True, True)
+    executeOSCommandOrFile("sudo mkdir -p /etc/resolver", True, True)
+    executeOSCommandOrFile("sudo bash -c 'echo ""nameserver 127.0.0.1"" > /etc/resolver/massa'", True, True)
+
+    print("Restarting DNSMasq...")
+    executeOSCommandOrFile("sudo brew services restart dnsmasq", True, True)
+
+
+# Generic
+def downloadFile(url, filename):
+    logging.info("Downloading " + filename + "...")
+    try:
+        sslContext = None
+        if platform.system() != "Windows":
+            import certifi
+            sslContext = ssl.create_default_context(cafile=certifi.where()) 
+        with urllib.request.urlopen(url, context=sslContext) as response, open(filename, 'wb') as out_file:
+            shutil.copyfileobj(response, out_file)
+    except URLError as err:
+        logging.info("Failed to download " + filename + " :")
+        printErrorAndExit(err)
+    logging.info(filename + " downloaded successfully")
+
+def executeOSCommandOrFile(command, decodeBinary, shell=False):
+    process = subprocess.Popen(command,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            universal_newlines=decodeBinary,
+                            shell=shell)
+    stdout, stderr = process.communicate()
+
+    if stderr != None and stderr != "" and process.returncode != 0:
+        printErrorAndExit(f"Error encountered while executing : {command} :\n{stderr}")
+    return stdout
+
 def printErrorAndExit(error):
-    logging.error(error)
-    os.system("pause")
+    print(error)
+    if platform.system() == "Windows":
+        os.system("pause")
     os._exit(-1)
 
 def main():
     if not os.path.exists(THYRA_CONFIG_FOLDER_PATH):
         os.makedirs(THYRA_CONFIG_FOLDER_PATH)
-    if isAdmin() == False:
+    if platform.system() == "Windows" and isAdmin() == False:
         printErrorAndExit("Couldn't detect admin rights. Please execute this script as an administator.")
-    downloadFile(THYRA_URL, THYRA_FILENAME)
-    downloadFile(THYRA_APP, THYRA_APP_FILENAME)
+
+    setThyraGlobals()
+    setThyraAppGlobals()
+    setMKCertGlobals()
+
+    downloadFile(THYRA_SERVER_URL, THYRA_SERVER_FILENAME)
+    downloadFile(THYRA_APP_URL, THYRA_APP_FILENAME)
     try:
-        shutil.move(THYRA_FILENAME, USER_HOME_FOLDER)
-        shutil.move(THYRA_APP_FILENAME, USER_HOME_FOLDER)
+        os.chmod(THYRA_SERVER_FILENAME, 0o755)
+        os.chmod(THYRA_APP_FILENAME, 0o755)
+
+        if os.getcwd() != USER_HOME_FOLDER:
+            thyra_home_path = os.path.join(USER_HOME_FOLDER, THYRA_SERVER_FILENAME)
+            thyra_app_home_path = os.path.join(USER_HOME_FOLDER, THYRA_APP_FILENAME)
+            if os.path.exists(thyra_home_path):
+                os.remove(thyra_home_path)
+            if os.path.exists(thyra_app_home_path):
+                os.remove(thyra_app_home_path)
+
+            shutil.move(THYRA_SERVER_FILENAME, USER_HOME_FOLDER)
+            shutil.move(THYRA_APP_FILENAME, USER_HOME_FOLDER)
     except OSError as err:
-        printErrorAndExit(err)    
-    if os.path.exists(DEFAULT_ACRYLIC_PATH):
-        logging.info("Acrylic DNS Proxy is already installed")
+        logging.error("Error while installing Thyra:")
+        printErrorAndExit(err)
+
+    if platform.system() == "Windows":
+        if os.path.exists(DEFAULT_ACRYLIC_PATH):
+            logging.info("Acrylic DNS Proxy is already installed")
+        else:
+            downloadFile(ACRYLIC_DNS_PROXY_URL, ACRYLIC_DNS_PROXY_FILENAME)
+            unzipAcrylic()
+            executeOSCommandOrFile(DEFAULT_ACRYLIC_PATH + "\InstallAcrylicService.bat", True)
+            setupDNS()
+        configureAcrylic()
+    elif platform.system() == "Darwin":
+        runningDNS = executeOSCommandOrFile("sudo lsof -i :53 | sed -n 2p | sed 's/[[:space:]].*$//'", True, True)
+        runningDNS = runningDNS[:-1]
+        if runningDNS == "dnsmasq":
+            logging.info("dnsmasq is already installed")
+            configureDNSMasq()
+        elif runningDNS == "":
+            logging.info("Installing dnsmasq...")
+            executeOSCommandOrFile("brew install dnsmasq", False)
+            configureDNSMasq()
+        else:
+            logging.info(runningDNS)
+            printErrorAndExit("Unsupported DNS server")
     else:
-        downloadFile(ACRYLIC_DNS_PROXY_URL, ACRYLIC_DNS_PROXY_FILENAME)
-        unzipAcrylic()
-        executeOSCommandOrFile(DEFAULT_ACRYLIC_PATH + "\InstallAcrylicService.bat", True)
-        setupDNS()
-    configureAcrylic()
-    setupMkCerts()
-    THYRA_APP_PATH = os.path.join(USER_HOME_FOLDER, THYRA_APP_FILENAME)
+        printErrorAndExit("Unsupported platform: " + platform.system())
+
+    generateCertificate()
+
     logging.info("Thyra has been successfully installed! Executable is located at : " + USER_HOME_FOLDER)
+
+    THYRA_APP_PATH = os.path.join(USER_HOME_FOLDER, THYRA_APP_FILENAME)
     if os.path.exists(THYRA_APP_PATH):
-        executeOSCommandOrFile(THYRA_APP_PATH, True)
+        subprocess.Popen([THYRA_APP_PATH], start_new_session=True)
         logging.info("You can start using thyra from the menu bar located on the bottom of your screen")
-    os.system("pause")
+
+    if platform.system() == "Windows":
+        os.system("pause")
     os._exit(0)
 
 main()
