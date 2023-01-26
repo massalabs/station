54be45095200c9d6c1858f9de7c8126811fa6533
0xMazout <lf@massa.net>
2023-01-26 14:56:54 +0100

    Last updates Del Tooltip atm

--- web/plugin-manager/src/components/pluginBlock.tsx
+++ web/plugin-manager/src/components/pluginBlock.tsx
@@ -6,6 +6,7 @@ export type PluginProps = {
     description: string;
     version: string;
     online: boolean;
+    updateAvailable: boolean;
 };
 
 function PluginBlock(props: PluginProps) {
@@ -17,33 +18,56 @@ function PluginBlock(props: PluginProps) {
         }
     }
 
-    return (
-        <div className="bg-slate-800 max-w-lg w-96 p-3 m-4 text-white rounded-md">
-            <div className="flex flex-row ">
-                <img className="w-10 h-10 mx-2 top" src={props.logo} alt="Plugin Logo" />
-                <div className="flex flex-col">
-                  <h1 className="font-bold text-center">{minimizeString(props.name, 90)}</h1>
-                  <p className="font-light">{minimizeString(props.description, 100)}</p>
+    function playStatus() {
+        if (props.online) {
+            return <PlayCircleIcon className="w-6 h-6 text-green-500" />;
+        } else {
+            return <PlayCircleIcon className="w-6 h-6 text-red-500" />;
+        }
+    }
+    function updateStatus() {
+        if (props.updateAvailable) {
+            return (
+                <div>
+                    <ArrowPathIcon className="w-6 h-6 text-yellow-500" />
                 </div>
-            </div>
-            <div className="inline-flex h-full w-full self-end pt-7">
-                <div className="">
-                    <div className="inline-flex mx-auto ">
-                        <p className="font-light ml-3">V: {props.version}</p>
-                        <input type="checkbox" className="toggle toggle-success ml-3" />
+            );
+        } else {
+            return (
+                <div>
+                    <ArrowPathIcon className="w-6 h-6 text-green-500" />
+                </div>
+            );
+        }
+    }
+
+    return (
+        <section className="bg-slate-800 h-48 max-w-lg w-96 p-3 m-4 rounded-2xl">
+            <div className=" flex-row h-full text-white ">
+                <div className="flex">
+                    <img className="w-10 h-10 pt-3 mx-2" src={props.logo} alt="Plugin Logo" />
+                    <div className="w-full">
+                        <h1 className="font-bold">{minimizeString(props.name, 90)}</h1>
+                        <p className="font-light max-sm:text-sm">
+                            {minimizeString(props.description, 100)}
+                        </p>
+                    </div>
+                </div>
+                <div className="content-center flex-wrap flex ">
+                    <div className="flex w-full pt-7 justify-around items-center">
+                        <p className="font-light">V: {props.version}</p>
+
+                        <input type="checkbox" className="toggle toggle-success " />
+
+                        <button>{updateStatus()}</button>
+                        <button>{playStatus()}</button>
                         <button>
-                            <PlayCircleIcon className="w-6 h-6 ml-3" />
-                        </button>
-                        <button>
-                            <ArrowPathIcon className="w-6 h-6 ml-3" />
-                        </button>
-                        <button>
-                            <TrashIcon className="w-6 h-6 ml-3" />
+                            <TrashIcon className="w-6 h-6 " />
                         </button>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 }
 
--- web/plugin-manager/src/pages/Manager.tsx
+++ web/plugin-manager/src/pages/Manager.tsx
@@ -9,6 +9,7 @@ function Manager() {
         description: "T Becarefull",
         version: "1.0.0",
         online: true,
+        updateAvailable: true,
     };
     const mock2: PluginProps = {
         name: "Plugin 2",
@@ -16,20 +17,23 @@ function Manager() {
         description: "This is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description Becarefull",
         version: "1.0.0",
         online: true,
+        updateAvailable: false,
     };
     const mock3: PluginProps = {
         name: "Plugin 3",
         logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/1/15/Audi_logo.svg/1280px-Audi_logo.svg.png",
-        description: "This is a plugin Descripaaaaaaaaaaaaaaaaaaaaaaaaation BecarefullThis is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description Becarefull",
+        description: "This is a plugin Descriaaaaaaaaaaaaatio  aaaaaaaaaaa n  aaaaaaaaaaa BecarefullThis is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description Becarefull",
         version: "1.0.0",
         online: true,
+        updateAvailable: true,
     };
     const mock4: PluginProps = {
       name: "Plugin 4",
       logo: "https://upload.wikimedia.org/wikipedia/fr/thumb/1/15/Audi_logo.svg/1280px-Audi_logo.svg.png",
-      description: "This is a plugin Desaaaaaaaaaaaaaaaaaaaaaaaaacription BecarefullThis is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description Becarefull",
+      description: "This is a plugin Deaaaaaaaaaacription BecarefullThis is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description Becarefull",
       version: "1.0.0",
       online: true,
+      updateAvailable: false,
   };
   const mock5: PluginProps = {
     name: "Plugin 5",
@@ -37,6 +41,7 @@ function Manager() {
     description: "This is a plugin Description BessssssssssscarefullThis is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description Becarefull",
     version: "1.0.0",
     online: true,
+    updateAvailable: true,
 };
 const mock6: PluginProps = {
   name: "Plugin 5",
@@ -44,6 +49,7 @@ const mock6: PluginProps = {
   description: "This is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description BecarefullThis is a plugin Description Becarefull",
   version: "1.0.0",
   online: true,
+  updateAvailable: true,
 };
     const mocks = [mock, mock2, mock3, mock4, mock5, mock6];
     const plugins = mocks.map((mock) => <PluginBlock {...mock} />);
@@ -53,6 +59,7 @@ const mock6: PluginProps = {
                 <img className="max-h-6" src={massaLogoLight} alt="Thyra Logo" />
                 <h1 className="text-xl ml-6 font-bold text-white">Thyra</h1>
             </div>
+            {/* FlexWrap is blocking align content in Plugin Block*/}
             <div className="flex flex-row flex-wrap mx-auto max-w-6xl justify-center content-center">
               {plugins}
             </div>
