e7d80b61609008db0d520b098dab7b6a1c0df479
Pierre Seznec <ps@massa.net>
2023-02-08 11:11:13 +0100

    run go generate from CI

---
+++ gen-frontend.go
@@ -0,0 +1,5 @@
+package resource
+
+// Thyra react frontend
+//go:generate npm install --prefix web/thyra-frontend
+//go:generate npm run --prefix web/thyra-frontend build
--- gen.go
+++ gen.go
@@ -3,6 +3,3 @@ package resource
 // API swagger
 //nolint:lll
 //go:generate swagger generate server --quiet --target api/swagger/server --name thyra-server --spec api/swagger/server/restapi/resource/swagger.yml --exclude-main
-
-// Thyra react frontend
-//go:generate npm install --prefix web/thyra-frontend && npm run --prefix web/thyra-frontend build
