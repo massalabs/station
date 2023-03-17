792c20c4fb2a4d9115e5bc53d53fc81ff139f90d
Grégory Libert <gl@massa.net>
2023-03-17 11:59:40 +0100

    CR comments

--- pkg/resource/embed.go
+++ pkg/resource/embed.go
@@ -5,12 +5,12 @@ import (
 	"io/fs"
 )
 
-type embedResources struct {
+type embeddedResources struct {
 	fs           embed.FS
 	subDirectory string
 }
 
-func (r *embedResources) Read(file string) ([]byte, error) {
+func (r *embeddedResources) Read(file string) ([]byte, error) {
 	content, err := r.fs.ReadFile(r.subDirectory + "/" + file)
 	if err == fs.ErrNotExist {
 		return nil, ErrNotExist
@@ -23,8 +23,8 @@ func (r *embedResources) Read(file string) ([]byte, error) {
 	return content, nil
 }
 
-func NewEmbedStorer(fs embed.FS, subDirectory string) *embedResources {
-	return &embedResources{fs: fs, subDirectory: subDirectory}
+func NewEmbedStorer(fs embed.FS, subDirectory string) *embeddedResources {
+	return &embeddedResources{fs: fs, subDirectory: subDirectory}
 }
 
-var _ Reader = new(embedResources)
+var _ Reader = new(embeddedResources)
