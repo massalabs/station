30af382bb460fe972beed387551ece1c0ef8d75b
0xMazout <lf@massa.net>
2023-03-31 15:33:23 +0200

    Change the - to _ in the return of API . Because in ECMA Script you cannot define a variable with - in it.

--- api/swagger/server/restapi/embedded_spec.go
+++ api/swagger/server/restapi/embedded_spec.go
@@ -1722,10 +1722,10 @@ func init() {
             "linux": {
               "$ref": "#/definitions/File"
             },
-            "macos-amd64": {
+            "macos_amd64": {
               "$ref": "#/definitions/File"
             },
-            "macos-arm64": {
+            "macos_arm64": {
               "$ref": "#/definitions/File"
             },
             "windows": {
@@ -3572,10 +3572,10 @@ func init() {
             "linux": {
               "$ref": "#/definitions/File"
             },
-            "macos-amd64": {
+            "macos_amd64": {
               "$ref": "#/definitions/File"
             },
-            "macos-arm64": {
+            "macos_arm64": {
               "$ref": "#/definitions/File"
             },
             "windows": {
@@ -3603,10 +3603,10 @@ func init() {
         "linux": {
           "$ref": "#/definitions/File"
         },
-        "macos-amd64": {
+        "macos_amd64": {
           "$ref": "#/definitions/File"
         },
-        "macos-arm64": {
+        "macos_arm64": {
           "$ref": "#/definitions/File"
         },
         "windows": {
