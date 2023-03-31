4818ab7fd071c490db84b50e983cd7e6bee48ba0
0xMazout <lf@massa.net>
2023-03-31 15:06:40 +0200

    Update title link

--- web/plugin-manager/src/components/Header.tsx
+++ web/plugin-manager/src/components/Header.tsx
@@ -4,16 +4,10 @@ import logoLight from "../assets/logo/logoLight.svg";
 import logoDark from "../assets/logo/logoDark.svg";
 
 const Header = () => {
-    const isThemeLight = UIStore.useState((s) => (s.theme === "light"));
+    const isThemeLight = UIStore.useState((s) => s.theme === "light");
     return (
-        <div className="flex p-12 items-center content-center justify-between ">
-            <button onClick={() => window.open("/thyra/home")}>
-                <img
-                    className="max-h-6"
-                    src={isThemeLight ? logoLight : logoDark}
-                    alt="Thyra Logo"
-                />
-            </button>
+        <div className="flex p-12 items-center content-center justify-between cursor-pointer ">
+            <img onClick={() => window.open("/thyra/home")} className="max-h-6" src={isThemeLight ? logoLight : logoDark} alt="Thyra Logo" />
             {toggleTheme()}
         </div>
     );
