2f8defbba30f43ffc62a914ee4a86a4a4f4810ee
Nathan Seva <thykof@protonmail.ch>
2023-05-04 14:13:58 +0200

    cherry-pick: update wallet url: /api/accounts

--- int/api/html/front/website.js
+++ int/api/html/front/website.js
@@ -147,7 +147,7 @@ function changeDefaultWallet(event) {
 
 async function getWallets() {
     axios
-        .get("/thyra/plugin/massalabs/wallet/rest/wallet")
+        .get("/thyra/plugin/massalabs/wallet/api/accounts")
         .then((resp) => {
             if (resp) {
                 gWallets = resp.data;
