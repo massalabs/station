bde712012e784db67be2aa9c6c1704068a1a8a41
Pierre Seznec <petterseznec@gmail.com>
2023-05-04 14:36:48 +0200

    remove dead code

--- bridge/dart/lib/thyra_bridge_dart.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library thyra_bridge_dart;
-
-export 'src/thyra_bridge_dart_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
