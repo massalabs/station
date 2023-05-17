19d17f78076e4f35824c84b36397766866dd427b
thomas-senechal <thomas.senechal@pm.me>
2023-05-17 10:01:12 +0200

    Add exception catching in main function.

--- installer/windows/build_installer.py
+++ installer/windows/build_installer.py
@@ -341,6 +341,14 @@ if __name__ == "__main__":
             )
             sys.exit(1)
 
-    install_dependencies()
-    build_installer()
+    try:
+        install_dependencies()
+    except Exception as err:
+        print("Error installing dependencies: ", err)
+        sys.exit(1)
+    try:
+        build_installer()
+    except Exception as err:
+        print("Error building installer: ", err)
+        sys.exit(1)
     sys.exit(0)
