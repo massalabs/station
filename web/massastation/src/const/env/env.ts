99dc189e9ff3d055b18fc833aa14a5ea5cdb359b
Pierre <pierregue75@gmail.com>
2023-06-13 17:00:31 +0200

    init mirage & set get plugin list

---
+++ web/massastation/src/const/env/env.ts
@@ -0,0 +1,6 @@
+export const ENV = {
+  DEV: 'dev',
+  TEST: 'test',
+  MASSASTATION: 'massastation',
+  STANDALONE: 'standalone',
+};
---
+++ web/massastation/src/mirage/server.js
@@ -0,0 +1,47 @@
+import { createServer, Model, Factory } from 'miragejs';
+import { faker } from '@faker-js/faker';
+import { ENV } from '../';
+const generatePlugin = () => {
+  const name = faker.lorem.word();
+  return {
+    description: faker.lorem.sentence(),
+    home: `/plugin/massalabs/${name.toLowerCase()}/`,
+    id: faker.number.int(),
+    logo: `/plugin/massalabs/${name.toLowerCase()}/logo.svg`,
+    name,
+    status: 'Up',
+    version: faker.system.semver(),
+  };
+};
+
+/**
+ *
+ * @param environment
+ */
+function mockServer(environment = ENV.DEV) {
+  const mockedServer = createServer({
+    environment,
+    models: {
+      plugin: Model,
+    },
+    factories: {
+      plugin: Factory.extend({
+        plugin: generatePlugin(),
+      }),
+    },
+    seeds(server) {
+      server.createList('plugin', 5);
+    },
+    routes() {
+      this.namespace = import.meta.env.VITE_BASE_API;
+      this.get('/plugin-manager', (schema) => {
+        let { models: plugins } = schema.plugins.all();
+        return plugins;
+      });
+    },
+  });
+
+  return mockedServer;
+}
+
+export default mockServer;
