ea863e8defd1992f32dd9326700bd6964360fc3e
Pierre <pierregue75@gmail.com>
2023-06-13 17:26:10 +0200

    refacto sendForm & advanced

--- web/massastation/src/const/env/env.ts
+++ web/massastation/src/const/env/env.ts
@@ -1,6 +1,5 @@
 export const ENV = {
   DEV: 'dev',
   TEST: 'test',
-  MASSASTATION: 'massastation',
   STANDALONE: 'standalone',
 };
--- web/massastation/src/mirage/server.js
+++ web/massastation/src/mirage/server.js
@@ -1,25 +1,14 @@
 import { createServer, Model, Factory } from 'miragejs';
 import { faker } from '@faker-js/faker';
-import { ENV } from '../';
-const generatePlugin = () => {
-  const name = faker.lorem.word();
-  return {
-    description: faker.lorem.sentence(),
-    home: `/plugin/massalabs/${name.toLowerCase()}/`,
-    id: faker.number.int(),
-    logo: `/plugin/massalabs/${name.toLowerCase()}/logo.svg`,
-    name,
-    status: 'Up',
-    version: faker.system.semver(),
-  };
-};
+import { ENV } from '../const/env/env';
 
 /**
  *
  * @param environment
  */
-// eslint-disable-next-line jsdoc/require-jsdoc
 function mockServer(environment = ENV.DEV) {
+  const commonVariable = 'Common Value';
+
   const mockedServer = createServer({
     environment,
     models: {
@@ -27,14 +16,33 @@ function mockServer(environment = ENV.DEV) {
     },
     factories: {
       plugin: Factory.extend({
-        plugin: generatePlugin(),
+        name() {
+          return faker.lorem.word();
+        },
+        description() {
+          return faker.lorem.sentence();
+        },
+        home() {
+          const name = this.name.toLowerCase();
+          return `/plugin/massalabs/${name}/`;
+        },
+        id() {
+          return faker.number.int();
+        },
+        logo() {
+          const name = this.name.toLowerCase();
+          return `/plugin/massalabs/${name}/logo.svg`;
+        },
+        status: 'Up',
+        version() {
+          return faker.system.semver();
+        },
       }),
     },
     seeds(server) {
       server.createList('plugin', 5);
     },
     routes() {
-      this.namespace = import.meta.env.VITE_BASE_API;
       this.get('/plugin-manager', (schema) => {
         let { models: plugins } = schema.plugins.all();
         return plugins;
