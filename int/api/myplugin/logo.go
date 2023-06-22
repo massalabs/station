762eeb39166fac8dbfc78c83aabc01b5a6d80f47
Pierre <pierregue75@gmail.com>
2023-06-22 13:53:38 +0200

    update yml & add handler

---
+++ int/api/myplugin/logo.go
@@ -0,0 +1,65 @@
+package myplugin
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+	"github.com/massalabs/station/api/swagger/server/models"
+	"github.com/massalabs/station/api/swagger/server/restapi/operations"
+	"github.com/massalabs/station/int/api/utils"
+	"github.com/massalabs/station/pkg/plugin"
+)
+
+func newLogo(manager *plugin.Manager) operations.PluginManagerGetLogoHandler {
+	return &logo{manager: manager}
+}
+
+type logo struct {
+	manager *plugin.Manager
+}
+
+type imageResponder struct {
+	payload io.Reader
+}
+
+func (ir *imageResponder) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
+	io.Copy(rw, ir.payload)
+}
+
+func (l *logo) Handle(param operations.PluginManagerGetLogoParams) middleware.Responder {
+	plgn, err := l.manager.Plugin(param.ID)
+	if err != nil {
+		return operations.NewPluginManagerGetLogoNotFound().WithPayload(
+			&models.Error{Code: errorCodePluginUnknown, Message: fmt.Sprintf("plugin %s not found", param.ID)})
+	}
+	info := plgn.Information()
+	logoPath := info.Logo
+
+	// Open the logo file
+	logoFile, err := os.Open(logoPath)
+	if err != nil {
+		log.Printf("Error opening logo file: %s", err)
+		if err != nil {
+			return operations.NewPluginManagerExecuteCommandNotFound().WithPayload(
+				&models.Error{Code: errorCodePluginLogoNotFound, Message: fmt.Sprintf("plugin %s logo not found", param.ID)})
+		}
+	}
+	defer logoFile.Close()
+
+	// Read the logo file
+	logoData, err := ioutil.ReadAll(logoFile)
+	if err != nil {
+		log.Printf("Error reading logo file: %s", err)
+		return operations.NewPluginManagerGetLogoInternalServerError().WithPayload(
+			&models.Error{Code: errorCodePluginUnknown, Message: fmt.Sprintf("get plugin logo error: %s", err.Error())})
+
+	}
+
+	return utils.NewCustomResponder(logoData, utils.ContentType(logoPath), http.StatusOK)
+}
