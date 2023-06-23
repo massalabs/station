a34f2c0dafc2ef1f7ca46db921e9dc2c92bca83c
BRAHIMI Naoufal <nb@massa.net>
2023-06-23 11:10:39 +0200

    split the config from network code

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,11 +2,66 @@ package config
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
-//nolint:gochecknoglobals
-var Version = "dev"
+var (
+	appConfig     AppConfig
+	knownNetworks map[string]NetworkConfig
+	Version       = "dev"
+)
+
+// init initializes the known networks and the app configuration.
+func init() {
+	// Load network configuration from "config_network.yaml"
+	initNetworksData, err := LoadConfig("config_network.yaml")
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
+	SetNetworks(initNetworksData)
+
+	// Get AppConfig for the selected network (BuildNet)
+	initConfig, err := GetAppConfig(BuildNet)
+	if err != nil {
+		log.Fatal("Failed to get app configuration:", err)
+	}
+
+	SetAppConfig(initConfig)
+}
+
+// Config returns the current app configuration.
+func Config() AppConfig {
+	return appConfig
+}
+
+// SetAppConfig sets the app configuration.
+func SetAppConfig(config AppConfig) {
+	appConfig = config
+}
+
+// SetNetworks sets the known networks configuration.
+func SetNetworks(networks map[string]NetworkConfig) {
+	knownNetworks = networks
+}
+
+// Networks returns the map of network configurations.
+func Networks() map[string]NetworkConfig {
+	return knownNetworks
+}
+
+// SwitchNetworkTO updates the app configuration based on the selected network.
+func SwitchNetworkTO(selectedNetwork NetworkOption) {
+	// Get AppConfig for the selected network
+	newConfig, err := GetAppConfig(selectedNetwork)
+	if err != nil {
+		log.Fatal("Failed to get app configuration:", err)
+	}
+
+	// Set the new configuration
+	SetAppConfig(newConfig)
+}
 
 // GetConfigDir returns the config directory for the current OS.
 func GetConfigDir() (string, error) {
--- pkg/config/network.go
+++ pkg/config/network.go
@@ -3,58 +3,19 @@ package config
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 
 	"gopkg.in/yaml.v2"
 )
+
 const (
 	MassaStationURL = "station.massa"
 )
+
 type AppConfig struct {
 	Network    string
 	NodeURL    string
 	DNSAddress string
 }
-
-var networksData map[string]NetworkConfig
-var appConfig AppConfig
-
-func init() {
-	// Load network configuration from "config_network.yaml"
-	var err error
-	networksData, err = LoadConfig("config_network.yaml")
-	if err != nil {
-		log.Fatal("Failed to load configuration:", err)
-	}
-
-	// Get AppConfig for the selected network (BuildNet)
-	appConfig, err = GetAppConfig(BuildNet)
-	if err != nil {
-		log.Fatal("Failed to get app configuration:", err)
-	}
-}
-
-
-func Config() AppConfig {
-	return appConfig
-}
-
-func Networks() map[string]NetworkConfig {
-	return networksData
-}
-
-func UpdateConfig(selectedNetwork NetworkOption) {
-	// Get AppConfig for the selected network
-	newConfig, err := GetAppConfig(selectedNetwork)
-	if err != nil {
-		log.Fatal("Failed to get app configuration:", err)
-	}
-
-	// Update appConfig with the new configuration
-	appConfig = newConfig
-}
-
-
 type NetworkConfig struct {
 	DNS  string   `yaml:"DNS"`
 	URLs []string `yaml:"URLs"`
@@ -80,6 +41,7 @@ func (option NetworkOption) String() string {
 
 func GetNetworkOptions() []NetworkOption {
 	options := make([]NetworkOption, 0, len(Networks()))
+
 	for network := range Networks() {
 		switch network {
 		case "testnet":
@@ -90,6 +52,7 @@ func GetNetworkOptions() []NetworkOption {
 			options = append(options, LabNet)
 		}
 	}
+
 	return options
 }
 
@@ -103,6 +66,7 @@ func GetAppConfig(selectedNetwork NetworkOption) (AppConfig, error) {
 	}
 
 	appConfig := AppConfig{
+		// TODO add logique to choose which URL to use
 		NodeURL:    config.URLs[0],
 		DNSAddress: config.DNS,
 		Network:    selectedNetworkStr,
@@ -113,45 +77,20 @@ func GetAppConfig(selectedNetwork NetworkOption) (AppConfig, error) {
 
 // LoadConfig reads the YAML configuration file and returns a map of network configurations.
 // The keys of the map represent the network names, and the values contain the corresponding network configuration.
-// If the configuration file is successfully loaded and parsed, the map of network configurations is returned along with nil error.
-// If there is an error reading the file or parsing the YAML data, the function returns nil map and the encountered error.
 func LoadConfig(filename string) (map[string]NetworkConfig, error) {
 	var networksData map[string]NetworkConfig
 
 	// Read the YAML file
 	yamlFile, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read YAML file: %w", err)
 	}
 
 	// Unmarshal the YAML data into the configData variable
 	err = yaml.Unmarshal(yamlFile, &networksData)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to unmarshal YAML data: %w", err)
 	}
-	
+
 	return networksData, nil
-}
-
-// NodeURL returns a list of available URLs for a given network.
-// If the network is found in the configuration, it returns the corresponding URLs.
-// If the network is not found, it returns an empty list.
-func NodeURL(network string, networkConfig map[string]NetworkConfig) []string {
-	if networkConfig, ok := networkConfig[network]; ok {
-		return networkConfig.URLs
-	}
-
-	// Network not found, return an empty list
-	return []string{}
-}
-
-// DNSAddress returns the DNS address for a given network.
-// If the network is found in the configuration, it returns the corresponding DNS address.
-// If the network is not found, it returns an empty string.
-func DNSAddress(network string, networkConfig map[string]NetworkConfig) string {
-	if networkConfig, ok := networkConfig[network]; ok {
-		return networkConfig.DNS
-	}
-	// Network not found, return an empty string
-	return ""
 }
