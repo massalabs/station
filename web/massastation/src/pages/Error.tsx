74ff01bc6ebd604e81ce43d65e447dd4c1e3b97b
Nathan Seva <thykof@protonmail.ch>
2023-06-23 12:49:28 +0200

    fix error page

--- web/massastation/src/pages/Error.tsx
+++ web/massastation/src/pages/Error.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
 import Intl from '../i18n/i18n';
+import { routeFor } from '../utils';
 
 export function Error() {
   return (
-    <div id="error-page">
+    <div id="error-page" className="text-f-primary">
       <h1 className="mas-banner">{Intl.t('unexpected-error.title')}</h1>
       <p className="mas-body">{Intl.t('unexpected-error.description')}</p>
-      <Link to="index">{Intl.t('unexpected-error.link')}</Link>
+      <Link to={routeFor('index')} className="underline">
+        {Intl.t('unexpected-error.link')}
+      </Link>
     </div>
   );
 }
