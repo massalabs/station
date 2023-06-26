81befbfd848d6809a66c9afbbbdda1c000e8d91a
BRAHIMI Naoufal <nb@massa.net>
2023-06-26 23:40:02 +0200

    handle on change network in front

--- web/massastation/src/layouts/LayoutStation/LayoutStation.tsx
+++ web/massastation/src/layouts/LayoutStation/LayoutStation.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, useState, useEffect } from 'react';
 import { Dropdown, StationLogo, ThemeMode } from '@massalabs/react-ui-kit';
 import { URL } from '../../const/url/url';
 import { useResource } from '../../custom/api';
@@ -16,19 +16,43 @@ export function LayoutStation({
   activePage,
 }: LayoutStationProps) {
   const [selectedTheme, setSelectedTheme] = useState('theme-dark');
+  const [availableNetworks, setAvailableNetworks] = useState([]);
+  const [currentNetwork, setCurrentNetwork] = useState('');
 
   function handleSetTheme(theme: string) {
     setSelectedTheme(theme);
     onSetTheme?.(theme);
   }
+
   const { data: networkConfig = {} } = useResource<NetworksObject>(
     `${URL.PATH_NETWORKS}`,
   );
 
-  const availableNetworks = networkConfig.availableNetworks?.map((n) => ({
-    item: n,
-  }));
-  const currentNetwork = networkConfig.currentNetwork;
+  useEffect(() => {
+    if (networkConfig.availableNetworks) {
+      setAvailableNetworks(
+        networkConfig.availableNetworks.map((n) => ({ item: n })),
+      );
+    }
+    if (networkConfig.currentNetwork) {
+      setCurrentNetwork(networkConfig.currentNetwork);
+    }
+  }, [networkConfig]);
+
+  const handleNetworkChange = (selectedNetwork: string) => {
+    // Make the network switch request here
+    // Assuming you are using fetch() for API requests
+    fetch(`/network/${selectedNetwork}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response if necessary
+        console.log('Current Network:', data.currentNetwork);
+      })
+      .catch((error) => {
+        // Handle the error if necessary
+        console.error(error);
+      });
+  };
 
   return (
     <div
@@ -45,8 +69,9 @@ export function LayoutStation({
         <div className="flex justify-end items-start gap-20">
           <div className="w-64">
             <Dropdown
-              options={availableNetworks || []}
+              options={availableNetworks}
               select={currentNetwork}
+              onChange={handleNetworkChange}
             />
           </div>
           <ThemeMode onSetTheme={handleSetTheme} />
