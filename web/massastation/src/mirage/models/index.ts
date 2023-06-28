ca216f8256cd1b4b78d08ab43f6d9588786bcd22
Nathan Seva <thykof@protonmail.ch>
2023-06-28 12:26:09 +0200

    🔥 remove legacy frontends

--- web/massastation/src/mirage/models/index.ts
+++ web/massastation/src/mirage/models/index.ts
@@ -3,14 +3,13 @@ import { ModelDefinition } from 'miragejs/-types';
 import { AccountObject } from '../../models/AccountModel';
 import { DomainModel } from '../../models/DomainModel';
 import { NetworkModel } from '../../models/NetworkModel';
-import { IMassaPlugin } from '../../../../shared/interfaces/IPlugin';
-import { IMassaStore } from '../../../../shared/interfaces/IPlugin';
+import { MassaPluginModel, MassaStoreModel } from '@/models';
 
 const accountModel: ModelDefinition<AccountObject> = Model.extend({});
 const domainModel: ModelDefinition<DomainModel> = Model.extend({});
 const networkModel: ModelDefinition<NetworkModel> = Model.extend({});
-const pluginModel: ModelDefinition<IMassaPlugin> = Model.extend({});
-const storeModel: ModelDefinition<IMassaStore> = Model.extend({});
+const pluginModel: ModelDefinition<MassaPluginModel> = Model.extend({});
+const storeModel: ModelDefinition<MassaStoreModel> = Model.extend({});
 
 export const models = {
   account: accountModel,
---
+++ web/massastation/src/models/MassaStoreModel.ts
@@ -0,0 +1,24 @@
+export interface MassaStoreModel {
+  name: string;
+  author: string;
+  description: string;
+  version: string;
+  url: string;
+  logo: string;
+  file: pluginStoreItemFile;
+  os: string;
+}
+
+export interface pluginStoreItemFile {
+  url: string;
+  checksum: string;
+}
+
+export enum PluginStatus {
+  Down = 'Down',
+  Up = 'Up',
+  Starting = 'Starting',
+  Stopping = 'Stopping',
+  Error = 'Error',
+  NotInstalled = 'NotInstalled',
+}
