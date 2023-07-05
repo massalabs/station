ff8323228c5e87446bd8247fc2bf44e83f4fa77d
Thykof <ns@massa.net>
2023-07-05 12:14:59 +0200

    split store linux and darwin

---
+++ pkg/certificate/store/store_linux.go
@@ -0,0 +1,17 @@
+//go:build linux
+// +build linux
+
+package store
+
+import (
+	"crypto/x509"
+	"fmt"
+)
+
+func Add(_ *x509.Certificate) error {
+	return fmt.Errorf("not implemented")
+}
+
+func Delete(_ *x509.Certificate) error {
+	return fmt.Errorf("not implemented")
+}
