8e8dc535a47876753ae44921ca007e3596d73a33
Grégory Libert <gl@massa.net>
2023-07-06 15:54:45 +0200

    add test

---
+++ pkg/nss/certutilservice_unix_test.go
@@ -0,0 +1,40 @@
+package nss
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestCertUtilService_AddCA tests the AddCA method of CertUtilService.
+func TestCertUtilService_AddCA(t *testing.T) {
+	dbPath := "testdata"
+	certPath := "testdata/cert.pem"
+	certName := "testNSS"
+
+	runner, err := NewCertUtilRunner()
+	if err != nil {
+		t.Fatalf("Failed to create runner: %v", err)
+	}
+
+	service, err := NewCertUtilService(runner)
+	if err != nil {
+		t.Fatalf("Failed to create certutil service: %v", err)
+	}
+
+	// Adding the certificate.
+	err = service.AddCA(dbPath, certName, certPath)
+	assert.Nil(t, err)
+
+	// Verifying the certificate is present.
+	err = service.IsKnownCA(dbPath, certName)
+	assert.Nil(t, err)
+
+	// Removing the certificate.
+	err = service.DeleteCA(dbPath, certName)
+	assert.Nil(t, err)
+
+	// Verifying the certificate is no longer present.
+	err = service.IsKnownCA(dbPath, certName)
+	assert.Nil(t, err)
+}
---
+++ pkg/nss/manager_test.go
@@ -0,0 +1,134 @@
+package nss
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockCertUtilServicer is a mock of CertUtilServicer.
+type MockCertUtilServicer struct {
+	mock.Mock
+}
+
+func (m *MockCertUtilServicer) AddCA(path string, certName string, certPath string) error {
+	args := m.Called(path, certName, certPath)
+
+	//nolint:wrapcheck
+	return args.Error(0)
+}
+
+func (m *MockCertUtilServicer) DeleteCA(path string, certName string) error {
+	args := m.Called(path, certName)
+
+	//nolint:wrapcheck
+	return args.Error(0)
+}
+
+func (m *MockCertUtilServicer) IsKnownCA(path string, certName string) error {
+	args := m.Called(path, certName)
+
+	//nolint:wrapcheck
+	return args.Error(0)
+}
+
+// MockLogger is a mock of Logger.
+type MockLogger struct {
+	mock.Mock
+}
+
+func (m *MockLogger) Debugf(msg string, args ...interface{}) {
+	m.Called(msg, args)
+}
+
+func TestManager_AddCA(t *testing.T) {
+	mockCertUtilServicer := new(MockCertUtilServicer)
+	mockLogger := new(MockLogger)
+	m := NewManager([]string{"/path/to/db"}, mockCertUtilServicer, mockLogger)
+
+	mockCertUtilServicer.On("AddCA", "/path/to/db", "testCert", "/path/to/cert").Return(nil)
+	mockLogger.On("Debugf", mock.Anything, mock.Anything)
+
+	err := m.AddCA("testCert", "/path/to/cert")
+
+	assert.NoError(t, err)
+	mockCertUtilServicer.AssertExpectations(t)
+	mockLogger.AssertExpectations(t)
+}
+
+func TestManager_DeleteCA(t *testing.T) {
+	mockCertUtilServicer := new(MockCertUtilServicer)
+	mockLogger := new(MockLogger)
+	m := NewManager([]string{"/path/to/db"}, mockCertUtilServicer, mockLogger)
+
+	mockCertUtilServicer.On("DeleteCA", "/path/to/db", "testCert").Return(nil)
+	mockLogger.On("Debugf", mock.Anything, mock.Anything)
+
+	err := m.DeleteCA("testCert")
+
+	assert.NoError(t, err)
+	mockCertUtilServicer.AssertExpectations(t)
+	mockLogger.AssertExpectations(t)
+}
+
+func TestManager_HasCA(t *testing.T) {
+	mockCertUtilServicer := new(MockCertUtilServicer)
+	mockLogger := new(MockLogger)
+	m := NewManager([]string{"/path/to/db"}, mockCertUtilServicer, mockLogger)
+
+	mockCertUtilServicer.On("IsKnownCA", "/path/to/db", "testCert").Return(nil)
+	mockLogger.On("Debugf", mock.Anything, mock.Anything)
+
+	result := m.HasCA("testCert")
+
+	assert.True(t, result)
+	mockCertUtilServicer.AssertExpectations(t)
+	mockLogger.AssertExpectations(t)
+}
+
+func TestManager_AddCA_Error(t *testing.T) {
+	mockCertUtilServicer := new(MockCertUtilServicer)
+	mockLogger := new(MockLogger)
+	m := NewManager([]string{"/path/to/db"}, mockCertUtilServicer, mockLogger)
+
+	mockCertUtilServicer.On("AddCA", "/path/to/db", "testCert", "/path/to/cert").Return(errors.New("mock error"))
+	mockLogger.On("Debugf", mock.Anything, mock.Anything)
+
+	err := m.AddCA("testCert", "/path/to/cert")
+
+	assert.Error(t, err)
+	mockCertUtilServicer.AssertExpectations(t)
+	mockLogger.AssertExpectations(t)
+}
+
+func TestManager_DeleteCA_Error(t *testing.T) {
+	mockCertUtilServicer := new(MockCertUtilServicer)
+	mockLogger := new(MockLogger)
+	m := NewManager([]string{"/path/to/db"}, mockCertUtilServicer, mockLogger)
+
+	mockCertUtilServicer.On("DeleteCA", "/path/to/db", "testCert").Return(errors.New("mock error"))
+	mockLogger.On("Debugf", mock.Anything, mock.Anything)
+
+	err := m.DeleteCA("testCert")
+
+	assert.Error(t, err)
+	mockCertUtilServicer.AssertExpectations(t)
+	mockLogger.AssertExpectations(t)
+}
+
+func TestManager_HasCA_Error(t *testing.T) {
+	mockCertUtilServicer := new(MockCertUtilServicer)
+	mockLogger := new(MockLogger)
+	m := NewManager([]string{"/path/to/db"}, mockCertUtilServicer, mockLogger)
+
+	mockCertUtilServicer.On("IsKnownCA", "/path/to/db", "testCert").Return(errors.New("mock error"))
+	mockLogger.On("Debugf", mock.Anything, mock.Anything)
+
+	result := m.HasCA("testCert")
+
+	assert.False(t, result)
+	mockCertUtilServicer.AssertExpectations(t)
+	mockLogger.AssertExpectations(t)
+}
