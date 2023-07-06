1726517bf0da99557e627617974ef0b3c363e1b8
Grégory Libert <gl@massa.net>
2023-07-06 19:16:12 +0200

    Improve logger package

---
+++ pkg/logger/global.go
@@ -0,0 +1,139 @@
+package logger
+
+// Initialize the logger at package level.
+var (
+	//nolint:gochecknoglobals
+	logger *Logger
+)
+
+// InitializeGlobal initializes the global logger at package level.
+// Once initialized, the logger can be used by calling the directly the classic logging functions (Debug, Info...).
+func InitializeGlobal(path string) error {
+	var err error
+
+	logger, err = New(path)
+
+	return err
+}
+
+// Debug logs a message at the debug level.
+// This function takes a variable number of arguments and uses fmt.Sprint to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Debug(args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Debug(args...)
+}
+
+// Debug logs a message at the debug level.
+// This function takes a variable number of arguments and uses fmt.Sprintf to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Debugf(template string, args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Debugf(template, args...)
+}
+
+// Info logs a message at the info level.
+// This function takes a variable number of arguments and uses fmt.Sprint to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Info(args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Info(args...)
+}
+
+// Infof logs a message at the info level.
+// This function takes a variable number of arguments and uses fmt.Sprintf to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Infof(template string, args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Infof(template, args...)
+}
+
+// Warn logs a message at the warn level.
+// This function takes a variable number of arguments and uses fmt.Sprint to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Warn(args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Warn(args...)
+}
+
+// Warnf logs a message at the warn level.
+// This function takes a variable number of arguments and uses fmt.Sprintf to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Warnf(template string, args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Warnf(template, args...)
+}
+
+// Error logs a message at the error level.
+// This function takes a variable number of arguments and uses fmt.Sprint to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Error(args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Error(args...)
+}
+
+// Errorf logs a message at the error level.
+// This function takes a variable number of arguments and uses fmt.Sprintf to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Errorf(template string, args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Errorf(template, args...)
+}
+
+// Fatal logs a message at the fatal level and stops the program.
+// This function takes a variable number of arguments and uses fmt.Sprint to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Fatal(args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Fatal(args...)
+}
+
+// Fatalf logs a message at the fatal level and stops the program.
+// This function takes a variable number of arguments and uses fmt.Sprintf to construct the log message.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Fatalf(template string, args ...interface{}) {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	logger.Fatalf(template, args...)
+}
+
+// Close closes the logger.
+// This function should be called before the program exits.
+// Warning: Calling this function will cause all subsequent calls to the logger to panic.
+// Note: The global logger must be initialized before calling this function, otherwise it will panic.
+func Close() error {
+	if logger == nil {
+		panic("global logger not initialized")
+	}
+
+	return logger.Close()
+}
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,53 +1,81 @@
 package logger
 
 import (
-	"log"
+	"fmt"
 	"os"
-	"path/filepath"
 
-	"github.com/massalabs/station/pkg/dirs"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-//nolint:gochecknoglobals
-var Logger *zap.SugaredLogger
+// FileModeReadWriteReadRead is the file mode used to create the log file.
+// It is -rw-r--r--.
+const FileModeReadWriteReadRead = 0o644
 
-func NewLogger() *zap.SugaredLogger {
-	logDir, err := dirs.GetConfigDir()
+// Logger holds the logger instance.
+type Logger struct {
+	*zap.SugaredLogger
+	file *os.File
+}
+
+// Close closes the logger.
+// It must be called before the program exits.
+func (l *Logger) Close() error {
+	if l.SugaredLogger == nil {
+		return nil
+	}
+
+	err := l.SugaredLogger.Sync()
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("sync logger: %w", err)
 	}
 
-	logDirPath := filepath.Join(logDir, "logs")
-	logFilePath := filepath.Join(logDirPath, "massastation.log")
+	l.SugaredLogger = nil
 
-	// Create the log directory if it doesn't exist
-	if err := os.MkdirAll(logDirPath, os.ModePerm); err != nil {
-		log.Fatal(err)
+	err = l.file.Close()
+	if err != nil {
+		return fmt.Errorf("close log file: %w", err)
 	}
 
-	// Configure log encoder
+	return nil
+}
+
+// New creates and returns a new Logger instance.
+func New(path string) (*Logger, error) {
+	// Get the log level.
+	logLevelStr, err := LogLevel()
+	if err != nil {
+		return nil, fmt.Errorf("get log level: %w", err)
+	}
+
+	var logLevel zapcore.Level
+
+	err = logLevel.Set(logLevelStr)
+	if err != nil {
+		return nil, fmt.Errorf("set log level: %w", err)
+	}
+
+	// Configure log encoder.
 	encoderConfig := zap.NewProductionEncoderConfig()
 	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 
-	//nolint:gomnd
-	logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
+	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, FileModeReadWriteReadRead)
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("open log file: %w", err)
 	}
 
-	// Create the log core
+	// Create the log core.
 	fileWriteSyncer := zapcore.AddSync(logFile)
 	consoleWriteSyncer := zapcore.AddSync(os.Stdout)
 	core := zapcore.NewCore(
 		zapcore.NewConsoleEncoder(encoderConfig),
 		zapcore.NewMultiWriteSyncer(fileWriteSyncer, consoleWriteSyncer),
-		zapcore.InfoLevel)
+		zapcore.InfoLevel,
+	)
 
-	// Create the logger
+	// Create the logger.
 	logger := zap.New(core, zap.AddCaller())
 
-	return logger.Sugar()
+	return &Logger{SugaredLogger: logger.Sugar(), file: logFile}, nil
 }
