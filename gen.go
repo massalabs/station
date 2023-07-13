1dbd8736db2297d00db1fe99ca4b1510b264597f
Nathan Seva <thykof@protonmail.ch>
2023-07-13 10:42:11 +0200

    update go in CI 1.20.6 (#1002)
    * update go in CI 1.20.6
    
    * try fix ci
    
    * ci: golangci lint version latest
    
    * lint code
    
    * lint: check go mod
    
    * rename unused parameter
    
    * remove go mod check
    
    * cr

--- gen.go
+++ gen.go
@@ -1,5 +1,4 @@
 package resource
 
 // API swagger
-//nolint:lll
 //go:generate swagger generate server --quiet --target api/swagger/server --name massastation --spec api/swagger/server/restapi/resource/swagger.yml --exclude-main
