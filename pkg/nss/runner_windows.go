988aab32e8d792d704dd737cc4aca543a9d51c90
thomas-senechal <thomas.senechal@pm.me>
2023-07-25 16:50:08 +0200

    Implement CertUtilRunner for Windows.

--- pkg/nss/runner_windows.go
+++ pkg/nss/runner_windows.go
@@ -5,19 +5,45 @@ package nss
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 // CertUtilRunner encapsulates certutil commands.
-type CertUtilRunner struct{}
+type CertUtilRunner struct {
+	binaryPath string
+}
 
 // NewCertUtilRunner returns a new CertUtilRunner.
 // It returns an error if the certutil binary is not found.
 func NewCertUtilRunner() (*CertUtilRunner, error) {
-	return nil, fmt.Errorf("not implemented")
+	ex, err := os.Executable()
+	if err != nil {
+		return nil, fmt.Errorf("getting executable path: %w", err)
+	}
+
+	dir := filepath.Dir(ex)
+	bin := filepath.Join(dir, "certutil.exe")
+
+	_, err = os.Stat(bin)
+	if err != nil {
+		return nil, fmt.Errorf("failed to find certutil binary: %w", err)
+	}
+
+	return &CertUtilRunner{binaryPath: bin}, nil
+
 }
 
 // Run runs the certutil command with the given arguments.
-// It returns the combined output of stdout and stderr.
+// It returns the combined output of stdout and stderr in the error, if any.
 func (r *CertUtilRunner) Run(args ...string) error {
-	return fmt.Errorf("not implemented")
+	cmd := exec.Command(r.binaryPath, args...) // #nosec G204
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%s: %w", string(out), err)
+	}
+
+	return nil
 }
