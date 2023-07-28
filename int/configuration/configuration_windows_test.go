66e4a441e4026e97ba0ea640c29e219d98533a18
Nathan Seva <thykof@protonmail.ch>
2023-07-28 10:29:36 +0200

    Run unit tests in all OS (#1044)
    * use task in CI
    
    * run unit test in all OS
    
    * task install in install action
    
    * import package in windows_test
    
    * fix mockgen call
    
    * use filepath.Join in tests
    
    * inline matrix

--- int/configuration/configuration_windows_test.go
+++ int/configuration/configuration_windows_test.go
@@ -4,9 +4,12 @@
 package configuration
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPath_Windows(t *testing.T) {
