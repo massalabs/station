c36cd2c015b5159cf808e49c090f56a8152e1b0d
thomas-senechal <thomas.senechal@pm.me>
2023-07-28 10:32:16 +0200

    Apply CR suggestion.

--- pkg/nss/manager_windows.go
+++ pkg/nss/manager_windows.go
@@ -4,47 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 )
-
-// getFirefoxProfilePath returns the path to the Firefox default profile directory.
-// It will search for the first directory that ends with ".default-release" which is the directory that contains the NSS database.
-func getFirefoxProfilePath() (string, error) {
-	appData := os.Getenv("APPDATA")
-	if appData == "" {
-		return "", fmt.Errorf("failed to get APPDATA environment variable")
-	}
-
-	firefoxProfilesDir := filepath.Join(appData, "Mozilla", "Firefox", "Profiles")
-	_, err := os.Stat(firefoxProfilesDir)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return "", fmt.Errorf("failed to find Firefox profiles directory: %w", err)
-		}
-		return "", fmt.Errorf("failed to check Firefox profiles directory: %w", err)
-	}
-
-	profiles, err := os.ReadDir(firefoxProfilesDir)
-	if err != nil {
-		return "", fmt.Errorf("failed to read Firefox profiles directory: %w", err)
-	}
-
-	var defaultProfileDir string
-	for _, profile := range profiles {
-		if profile.IsDir() && strings.HasSuffix(profile.Name(), ".default-release") {
-			defaultProfileDir = profile.Name()
-			break
-		}
-	}
-
-	if defaultProfileDir == "" {
-		return "", fmt.Errorf("failed to find default Firefox profile directory")
-	}
-
-	nssDBPath := filepath.Join(firefoxProfilesDir, defaultProfileDir)
-
-	return nssDBPath, nil
-}
 
 // defaultNSSDatabasePaths returns the known NSS databases directories of a Windows operating system.
 func defaultNSSDatabasePaths() ([]string, error) {
@@ -53,13 +13,8 @@ func defaultNSSDatabasePaths() ([]string, error) {
 		return nil, fmt.Errorf("failed to get APPDATA environment variable")
 	}
 
-	nssDBPaths := []string{}
-
-	firefoxDBPath, err := getFirefoxProfilePath()
-	if err == nil {
-		// If we found the Firefox profile directory, we add it to the list of NSS databases.
-		// If we didn't find it, we just ignore it and continue because it means that Firefox is not installed.
-		nssDBPaths = append(nssDBPaths, firefoxDBPath)
+	nssDBPaths := []string{
+		filepath.Join(appData, "Mozilla", "Firefox", "Profiles", "*"),
 	}
 
 	return nssDBPaths, nil
