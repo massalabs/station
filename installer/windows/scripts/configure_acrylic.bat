e3c35367dcd3a3a4c7087c245a6fa5c944bc1bdf
Pierre <pierregue75@gmail.com>
2023-08-02 14:28:17 +0200

    test

--- installer/windows/scripts/configure_acrylic.bat
+++ installer/windows/scripts/configure_acrylic.bat
@@ -8,6 +8,18 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET "acrylic_config=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicHosts.txt"
 SET "acrylic_configuration=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicConfiguration.ini"
+SET "source_file=config.ini"
+SET "target_file=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicConfiguration.ini"
+
+:: Remove the line containing "LocalIPv4BindingAddress"
+findstr /V "LocalIPv4BindingAddress" "%acrylic_configuration%" > temp.txt
+
+:: Replace the original file with the temporary one
+MOVE /Y temp.txt "%acrylic_configuration%"
+
+:: Add the new LocalIPv4BindingAddress setting at the end of the file
+ECHO LocalIPv4BindingAddress=127.0.0.1 >> "%acrylic_configuration%"
+
 :: If it doesn't exist, create it
 if not exist "%acrylic_config%" (
     :: Create the file
@@ -23,28 +35,14 @@ if %errorlevel%==0 (
     EXIT 0
 )
 
+:: Replace the target file with the source file
+COPY /Y "%source_file%" "%target_file%"
+
 
 :: Add the TLD to the file
 ECHO: >> "%acrylic_config%"
 ECHO 127.0.0.1 *.massa >> "%acrylic_config%"
 ECHO ::1 *.massa >> "%acrylic_config%"
-
-:: get line with LocalIPv4BindingAddress=0.0.0.0 and set it to LocalIPv4BindingAddress=127.0.0.1
-FOR /F "delims=:" %%A IN ('findstr /n /c:"LocalIPv4BindingAddress=0.0.0.0" "%acrylic_configuration%"') DO SET line=%%A
-IF DEFINED line (
-    :: Create a temporary file
-    SET "temp_config=%temp%\temp_acrylic_config.ini"
-    :: Use `more` command to read the file line by line
-    FOR /F "tokens=1* delims=:" %%A IN ('more +0 "%acrylic_configuration%"') DO (
-        IF %%A EQU %line% (
-            ECHO LocalIPv4BindingAddress=127.0.0.1 >> "%temp_config%"
-        ) ELSE (
-            ECHO %%A:%%B >> "%temp_config%"
-        )
-    )
-    :: Replace the original configuration file with the temporary one
-    MOVE /Y "%temp_config%" "%acrylic_configuration%"
-)
 
 :: Restart the Acrylic DNS Proxy Service
 NET STOP "AcrylicDNSProxySvc"
