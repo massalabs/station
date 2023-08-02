25093713e009eb858ad9cc64482ca1a5138a7020
Pierre <pierregue75@gmail.com>
2023-08-02 14:14:51 +0200

    change acrylic config

--- installer/windows/scripts/configure_acrylic.bat
+++ installer/windows/scripts/configure_acrylic.bat
@@ -7,7 +7,7 @@ ECHO Executing configure_acrylic.bat
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 SET "acrylic_config=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicHosts.txt"
-
+SET "acrylic_configuration=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicConfiguration.ini"
 :: If it doesn't exist, create it
 if not exist "%acrylic_config%" (
     :: Create the file
@@ -28,6 +28,23 @@ if %errorlevel%==0 (
 ECHO: >> "%acrylic_config%"
 ECHO 127.0.0.1 *.massa >> "%acrylic_config%"
 ECHO ::1 *.massa >> "%acrylic_config%"
+
+:: get line with LocalIPv4BindingAddress=0.0.0.0 and set it to LocalIPv4BindingAddress=127.0.0.1
+FOR /F "delims=:" %%A IN ('findstr /n /c:"LocalIPv4BindingAddress=0.0.0.0" "%acrylic_configuration%"') DO SET line=%%A
+IF DEFINED line (
+    :: Create a temporary file
+    SET "temp_config=%temp%\temp_acrylic_config.ini"
+    :: Use `more` command to read the file line by line
+    FOR /F "tokens=1* delims=:" %%A IN ('more +0 "%acrylic_configuration%"') DO (
+        IF %%A EQU %line% (
+            ECHO LocalIPv4BindingAddress=127.0.0.1 >> "%temp_config%"
+        ) ELSE (
+            ECHO %%A:%%B >> "%temp_config%"
+        )
+    )
+    :: Replace the original configuration file with the temporary one
+    MOVE /Y "%temp_config%" "%acrylic_configuration%"
+)
 
 :: Restart the Acrylic DNS Proxy Service
 NET STOP "AcrylicDNSProxySvc"
