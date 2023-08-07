5c85d68f399a4bf6f261048d33738fcbc69a672a
Pierre GUÉVENEUX <83134981+Pierregvx@users.noreply.github.com>
2023-08-07 15:18:15 +0200

    466-massastation-on-windows-port-53-already-used-by-wsl (#1071)
    * change acrylic config
    
    * test
    
    * restore ci
    
    * cr
    
    * Update configure_acrylic.bat
    
    * move LocalIPv4BindingAddress set before the TLD check

--- installer/windows/scripts/configure_acrylic.bat
+++ installer/windows/scripts/configure_acrylic.bat
@@ -6,28 +6,33 @@ ECHO Executing configure_acrylic.bat
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET "acrylic_config=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicHosts.txt"
+SET "acrylic_config_host=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicHosts.txt"
+SET "acrylic_configuration=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicConfiguration.ini"
 
 :: If it doesn't exist, create it
-if not exist "%acrylic_config%" (
+if not exist "%acrylic_config_host%" (
     :: Create the file
-    ECHO # Acrylic DNS Proxy configuration file > "%acrylic_config%"
-    ECHO # For more information please visit http://mayakron.altervista.org >> "%acrylic_config%"
-    ECHO # >> "%acrylic_config%"
+    ECHO # Acrylic DNS Proxy configuration file > "%acrylic_config_host%"
+    ECHO # For more information please visit http://mayakron.altervista.org >> "%acrylic_config_host%"
+    ECHO # >> "%acrylic_config_host%"
 )
 
+:: change LocalIPv4BindingAddress to 127.0.0.1
+findstr /V "LocalIPv4BindingAddress" "%acrylic_configuration%" > %acrylic_configuration%
+ECHO. >> "%acrylic_configuration%"
+ECHO LocalIPv4BindingAddress=127.0.0.1 >> "%acrylic_configuration%"
+
 :: Check if the TLD is already in the file
-FINDSTR /c:".massa" "%acrylic_config%" >nul 2>&1
+FINDSTR /c:".massa" "%acrylic_config_host%" >nul 2>&1
 if %errorlevel%==0 (
     ECHO TLD already in the file
     EXIT 0
 )
 
-
 :: Add the TLD to the file
-ECHO: >> "%acrylic_config%"
-ECHO 127.0.0.1 *.massa >> "%acrylic_config%"
-ECHO ::1 *.massa >> "%acrylic_config%"
+ECHO: >> "%acrylic_config_host%"
+ECHO 127.0.0.1 *.massa >> "%acrylic_config_host%"
+ECHO ::1 *.massa >> "%acrylic_config_host%"
 
 :: Restart the Acrylic DNS Proxy Service
 NET STOP "AcrylicDNSProxySvc"
