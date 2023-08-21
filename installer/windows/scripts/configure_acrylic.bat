6802b4e82241320c548cc3bc5d15ac965898f229
Thomas Sénéchal <44696378+thomas-senechal@users.noreply.github.com>
2023-08-21 12:02:49 +0200

    977 installation install massa station on windows on any disk (#1101)
    * Get Acrylic Path from Args in configure_acrylic.bat.
    
    * Fix LocalIPv4BindingAddress handling in configure_acrylic.bat.

--- installer/windows/scripts/configure_acrylic.bat
+++ installer/windows/scripts/configure_acrylic.bat
@@ -1,40 +1,57 @@
 @ECHO OFF
 
-:: Set Acrylic DNS to resolve `.massa` TLD to localhost
+@REM Set Acrylic DNS to resolve `.massa` TLD to localhost
 
 ECHO Executing configure_acrylic.bat
 
+if "%~1"=="" (
+    ECHO Please provide the path to Acrylic DNS Proxy installation folder
+    EXIT 1
+)
+
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET "acrylic_config_host=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicHosts.txt"
-SET "acrylic_configuration=C:\Program Files (x86)\Acrylic DNS Proxy\AcrylicConfiguration.ini"
+@REM Get Acrylic Path from command line argument
+SET acrylic_path=%~1
 
-:: If it doesn't exist, create it
+SET acrylic_config_host=%acrylic_path%\AcrylicHosts.txt
+SET acrylic_configuration=%acrylic_path%\AcrylicConfiguration.ini
+
+@REM AcrylicHosts.txt file does not exist, we create it
 if not exist "%acrylic_config_host%" (
-    :: Create the file
     ECHO # Acrylic DNS Proxy configuration file > "%acrylic_config_host%"
     ECHO # For more information please visit http://mayakron.altervista.org >> "%acrylic_config_host%"
     ECHO # >> "%acrylic_config_host%"
 )
 
-:: change LocalIPv4BindingAddress to 127.0.0.1
-findstr /V "LocalIPv4BindingAddress" "%acrylic_configuration%" > %acrylic_configuration%
-ECHO. >> "%acrylic_configuration%"
-ECHO LocalIPv4BindingAddress=127.0.0.1 >> "%acrylic_configuration%"
+@REM If LocalIPv4BindingAddress is set to default 0.0.0.0, we set it to 127.0.0.1 for backward compatibility with Windows 10
+FINDSTR /c:"LocalIPv4BindingAddress=127.0.0.1" "%acrylic_configuration%" >nul 2>&1
+if %errorlevel%==0 (
+    ECHO LocalIPv4BindingAddress already set to 127.0.0.1, skipping
+) else (
+    ECHO LocalIPv4BindingAddress not set to 127.0.0.1, setting it
+    powershell -Command "Get-Content '%acrylic_configuration%' | %%{$_ -replace 'LocalIPv4BindingAddress=0.0.0.0','LocalIPv4BindingAddress=127.0.0.1'} | Out-File -Encoding UTF8 '%acrylic_configuration%.tmp'"
+    FINDSTR /c:"LocalIPv4BindingAddress=127.0.0.1" "%acrylic_configuration%.tmp" >nul 2>&1
+    if !errorlevel!==0 (
+        MOVE /Y "%acrylic_configuration%.tmp" "%acrylic_configuration%"
+    ) else (
+        ECHO Failed to set LocalIPv4BindingAddress to 127.0.0.1, aborting this step
+    )
+)
 
-:: Check if the TLD is already in the file
+@REM If .massa TLD is already in AcrylicHosts.txt, we can exit
 FINDSTR /c:".massa" "%acrylic_config_host%" >nul 2>&1
 if %errorlevel%==0 (
     ECHO TLD already in the file
     EXIT 0
 )
 
-:: Add the TLD to the file
+@REM Add .massa TLD to AcrylicHosts.txt
 ECHO: >> "%acrylic_config_host%"
 ECHO 127.0.0.1 *.massa >> "%acrylic_config_host%"
 ECHO ::1 *.massa >> "%acrylic_config_host%"
 
-:: Restart the Acrylic DNS Proxy Service
+@REM Restart Acrylic DNS Proxy Service
 NET STOP "AcrylicDNSProxySvc"
 NET START "AcrylicDNSProxySvc"
 if %errorlevel% NEQ 0 (
