087a4e2b874f5c06d30d648a539234b06b199500
Thomas Sénéchal <44696378+thomas-senechal@users.noreply.github.com>
2023-10-05 12:53:41 +0200

    Apply suggestions from code review
    Co-authored-by: Grégory Libert <gl@massa.net>

--- installer/windows/scripts/add_station_to_hosts.bat
+++ installer/windows/scripts/add_station_to_hosts.bat
@@ -4,14 +4,14 @@ ECHO Executing add_station_to_hosts.bat
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-@REM Adds station.massa redirection to Windows hosts file if not already present
+@REM Adds station.massa host to Windows hosts file if not already present
 
 SET windows_hosts_file=%windir%\System32\drivers\etc\hosts
 
 @REM If station.massa is already in Windows hosts file, we can exit
 FINDSTR /c:"127.0.0.1 station.massa" "%windows_hosts_file%" >nul 2>&1
 if %errorlevel%==0 (
-    ECHO TLD already in the file
+    ECHO station.massa host already configured
     EXIT 0
 )
 
