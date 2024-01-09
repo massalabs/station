ce20d11050c2f6e3a09d3da41bf958636325411d
Nathan Seva <thykof@protonmail.ch>
2024-01-09 11:49:09 +0100

    web: remove unused account store (#1313)

--- web/massastation/src/store/store.ts
+++ web/massastation/src/store/store.ts
@@ -5,15 +5,10 @@ import { create } from 'zustand';
 
 // LOCALS
 import configStore, { ConfigStoreState } from './configStore';
-import accountStore, { AccountStoreState } from './accountStore';
 import networkStore, { NetworkStoreState } from './networkStore';
 
 export const useConfigStore = create<ConfigStoreState>((...obj) => ({
   ...configStore(...obj),
-}));
-
-export const useAccountStore = create<AccountStoreState>((...obj) => ({
-  ...accountStore(...obj),
 }));
 
 export const useNetworkStore = create<NetworkStoreState>((...obj) => ({
