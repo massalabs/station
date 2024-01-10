2987b465305a729250ddccb1ad09eefec5b58d38
Nathan Seva <thykof@protonmail.ch>
2024-01-10 18:10:57 +0100

    fix tests

--- int/api/massa/node_test.go
+++ int/api/massa/node_test.go
@@ -20,7 +20,7 @@ func TestNodeStatus(t *testing.T) {
 		responseTypes, ok := response.(*operations.GetNodeOK)
 		assert.True(t, ok, "responsePayload is not of type *operations.GetNodeOK")
 		responsePayload := responseTypes.Payload
-		assert.Equal(t, config.Network, responsePayload.Network)
+		assert.Equal(t, config.Network, *responsePayload.Network)
 		assert.Equal(t, config.ChainID, uint64(responsePayload.ChainID))
 		assert.Equal(t, config.NodeURL, *responsePayload.URL)
 	})
