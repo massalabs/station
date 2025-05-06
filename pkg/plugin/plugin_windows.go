8ab067d3862d109cd6d1f10ad36dfeed946567ca
thomas-senechal <thomas.senechal@pm.me>
2025-05-06 16:53:33 +0200

    Update Windows plugin shutdown mechanism to use CTRL_BREAK_EVENT for graceful termination

--- pkg/plugin/plugin_windows.go
+++ pkg/plugin/plugin_windows.go
@@ -13,19 +13,23 @@ import (
 
 // sendStopSignal sends an appropriate signal to gracefully stop a process on Windows
 func (p *Plugin) sendStopSignal() error {
-	err := windows.GenerateConsoleCtrlEvent(windows.CTRL_C_EVENT, uint32(p.command.Process.Pid))
+	// Windows doesn't have an exact equivalent to SIGTERM
+	// For CREATE_NEW_PROCESS_GROUP processes, CTRL_BREAK_EVENT is the closest equivalent
+	// The process group ID is the same as the process ID of the group leader
+	err := windows.GenerateConsoleCtrlEvent(windows.CTRL_BREAK_EVENT, uint32(p.command.Process.Pid))
 	if err != nil {
-		logger.Warnf("Failed to send Ctrl+C event to plugin %s: %v\n", p.ID, err)
+		logger.Warnf("Failed to send Ctrl+Break event to plugin %s: %v\n", p.ID, err)
 		return err
 	}
 
-	logger.Infof("Sent Ctrl+C event to plugin %s.\n", p.ID)
+	logger.Infof("Sent Ctrl+Break event to plugin %s.\n", p.ID)
 	return nil
 }
 
 // setupProcess configures Windows-specific process attributes
 func setupProcess(cmd *exec.Cmd) {
-	// Set CREATE_NEW_PROCESS_GROUP flag to make it respond to Ctrl+C
+	// CREATE_NEW_PROCESS_GROUP makes the process the leader of its own process group
+	// This allows it to receive CTRL_BREAK_EVENT signals
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
 	}
