5035a40452005e4e3fc3d48265c91e0229470bb4
Peterjah <ps@massa.net>
2025-05-13 11:17:46 +0200

    add executeSC endpoint

--- int/api/cmd/errors.go
+++ int/api/cmd/errors.go
@@ -1,11 +1,12 @@
 package cmd
 
 const (
-	errorSendOperation   = "Execute-0001"
-	errorInvalidArgs     = "Execute-0002"
-	errorInvalidFee      = "Execute-0003"
-	errorInvalidMaxGas   = "Execute-0004"
-	errorInvalidCoin     = "Execute-0005"
-	errorInvalidNickname = "Execute-0006"
-	errorInvalidMaxCoins = "Execute-0007"
+	errorSendOperation    = "Execute-0001"
+	errorInvalidArgs      = "Execute-0002"
+	errorInvalidFee       = "Execute-0003"
+	errorInvalidMaxGas    = "Execute-0004"
+	errorInvalidCoin      = "Execute-0005"
+	errorInvalidNickname  = "Execute-0006"
+	errorInvalidMaxCoins  = "Execute-0007"
+	errorInvalidDatastore = "Execute-0008"
 )
