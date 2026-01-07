f36be7db312bbb9093940f61b33f617782f44184
Peterjah <ps@massa.net>
2026-01-07 16:45:03 +0100

    fix fyne package to use manualy builded bin

--- gen.go
+++
@@ -1,4 +0,0 @@
-package resource
-
-// API swagger
-//go:generate swagger generate server --quiet --target api/swagger/server --name massastation --spec api/swagger/server/restapi/resource/swagger.yml --exclude-main
---
+++ scripts/gen.go
@@ -0,0 +1,4 @@
+package resource
+
+// API swagger
+//go:generate sh -c "cd .. && swagger generate server --quiet --target api/swagger/server --name massastation --spec api/swagger/server/restapi/resource/swagger.yml --exclude-main"
